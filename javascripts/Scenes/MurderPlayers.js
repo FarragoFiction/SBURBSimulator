99216bb27758cfc642e17f879e767341fffa39ef
JR <jennifer.schultz@ericsson.com>
2017-03-14 15:54:49 -0400

    debugging

--- javascripts/Scenes/MurderPlayers.js
+++ javascripts/Scenes/MurderPlayers.js
@@ -115,10 +115,10 @@ function MurderPlayers(){
 		if(murderer.isTroll == true || club.isTroll == true || club.isTroll == true){  //humans have regular romance, but if even one is a troll, this is romance.
 			var diSpriteBuffer = getBufferCanvas(document.getElementById("sprite_template"));
 			drawClub(diSpriteBuffer,1000) //Auspisticism
-			copyTmpCanvasToRealCanvasAtPos(canvas, diSpriteBuffer,300,0)
+			copyTmpCanvasToRealCanvasAtPos(canvas, diSpriteBuffer,350,0)
 		}
 		copyTmpCanvasToRealCanvasAtPos(canvas, pSpriteBuffer,-100,0)
-		copyTmpCanvasToRealCanvasAtPos(canvas, vSpriteBuffer,150,0) 
+		copyTmpCanvasToRealCanvasAtPos(canvas, vSpriteBuffer,100,0) 
 		copyTmpCanvasToRealCanvasAtPos(canvas, dSpriteBuffer,400,0) 
 	}
 	
