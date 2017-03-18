21ddf53c15fd9a3422faabea35b10317e96d63f1
Jenny <gtg287y@gmail.com>
2017-03-18 12:02:32 -0400

    hard strength recalibrated

--- javascripts/Scenes/KingPowerful.js
+++ javascripts/Scenes/KingPowerful.js
@@ -1,27 +1,30 @@
 function KingPowerful(){
-	this.canRepeat = false;	
+	this.canRepeat = false;
 	this.playerList = [];  //what players are already in the medium when i trigger?
-	
+
 	this.trigger = function(playerList){
 		this.playerList = playerList;
 		return kingStrength > hardStrength;
 	}
-	
+
 	this.renderContent = function(div){
 		div.append("<br>"+this.content());
 	}
-	
+
 	this.content = function(){
 		var badPrototyping = findBadPrototyping(this.playerList);
+		if(!badPrototyping){
+			badPrototyping = "glitchy piece of shit that is SBURB itself"
+		}
 		var ret = " At this point, the various prototypings from " +this.playerList.length;
 		ret += " players, especially the " + badPrototyping;
 		ret += ", have made the enemies  far too powerful. ";
-		
+
 		if(this.playerList.length < players.length){
 			ret += " Further prototypings will only serve to further strengthen the enemies. ";
 		}
 		return ret;
 	}
-	
+
 
 }
