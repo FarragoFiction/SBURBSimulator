f69034489b71308d4f31b84cd2dc4add6455e8e1
Jenny <gtg287y@gmail.com>
2017-03-18 11:56:30 -0400

    figured out why non players were flipping out about having time clones. labels were wrong

--- javascripts/Scenes/BeTriggered.js
+++ javascripts/Scenes/BeTriggered.js
@@ -1,11 +1,12 @@
 function BeTriggered(){
-	this.canRepeat = true;	
+	this.canRepeat = true;
 	this.playerList = [];  //what players are already in the medium when i trigger?
 	this.triggeredPlayers = [];
 	this.triggers = [];
 	this.trigger = function(playerList){
 		this.playerList = playerList;
 		this.triggeredPlayers = [];
+		this.triggers = [];
 		for(var i = 0; i<availablePlayers.length; i++){
 			var p = availablePlayers[i];
 			var trigger = this.IsPlayerTriggered(p)
@@ -16,11 +17,11 @@ function BeTriggered(){
 		}
 		return this.triggeredPlayers.length > 0;
 	}
-	
+
 	this.renderContent = function(div){
 		div.append("<br>"+this.content());
 	}
-	
+
 	this.IsPlayerTriggered = function(player){
 		//are any of your friends  dead?
 		var deadPlayers = findDeadPlayers(players);
@@ -28,14 +29,14 @@ function BeTriggered(){
 		var livePlayers = findLivingPlayers(players);
 		var worstEnemy = player.getWorstEnemyFromList(players);
 		var bestFriend = player.getBestFriendFromList(players);
-		
+
 		//small chance
 		if(deadPlayers.length > 0){
 			if(Math.seededRandom() > 0.9){
 				player.triggerLevel ++;
 				return deadPlayers.length +" dead players ";
 			}
-			
+
 			if(worstEnemy != null && !worstEnemy.dead && player.getRelationshipWith(worstEnemy).type() == player.getRelationshipWith(worstEnemy).badBig){
 				player.triggerLevel ++;
 				player.triggerLevel ++;
@@ -43,14 +44,14 @@ function BeTriggered(){
 				return deadPlayers.length + " players are dead (and that asshole the " + worstEnemy.htmlTitle() + " MUST be to blame) ";
 			}
 		}
-		
+
 		//bigger chance
 		if(deadFriends.length > 0){
 			if(Math.seededRandom() > 0.5){
 				player.triggerLevel ++;
 				return deadFriends.length + " dead friends";
 			}
-			
+
 			//if someone you have a crush on dies, you're triggered. period. (not necessarily gonna lose your shit, though.)
 			if(bestFriend != null && bestFriend.dead && player.getRelationshipWith(bestFriend).type() == player.getRelationshipWith(bestFriend).bigGood){
 				player.triggerLevel ++;
@@ -59,7 +60,7 @@ function BeTriggered(){
 				return " their dead crush, the " + bestFriend.htmlTitle() + " ";
 			}
 		}
-		
+
 		//huge chance, the dead outnumber the living.
 		if(deadPlayers.length > livePlayers.length){
 			if(Math.seededRandom() > 0.1){
@@ -68,27 +69,27 @@ function BeTriggered(){
 				player.triggerLevel ++;
 				return " how absolutely fucked they are ";
 			}
-		}	
-		
+		}
+
 		if(player.doomedTimeClones > 0 && Math.seededRandom() > .9){
 			return " their own doomed Time Clones ";
 		}
-		
+
 		if(player.denizenFaced && player.denizenDefeated && Math.seededRandom() > .95){
 			return " how terrifying " +player.getDenizen() + " was " ;
 		}
-		
+
 		//TODO have triggers specific to classes or aspects, like time players having to abort a timeline.
 		return " absolutely nothing ";
-		
+
 	}
-	
+
 	this.content = function(){
 		var ret = "";
 		for(var i = 0; i<this.triggeredPlayers.length; i++){
 			var p = this.triggeredPlayers[i];
 			removeFromArray(p, availablePlayers);
-			ret += " The " +p.htmlTitle() + " is currently too busy flipping the fuck out about " 
+			ret += " The " +p.htmlTitle() + " is currently too busy flipping the fuck out about "
 			ret += this.triggers[i] + " to be anything but a useless piece of gargbage. ";
 			p.triggerLevel ++;
 			if(p.triggerLevel > 5){
