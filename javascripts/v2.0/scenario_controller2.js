6aaeff8e43adb3e62d0dfbbcbaeccfc6b8ef1d2d
Jenny <gtg287y@gmail.com>
2017-03-22 00:16:57 -0400

    queens ring is based on native, not alien, players

--- javascripts/v2.0/scenario_controller2.js
+++ javascripts/v2.0/scenario_controller2.js
@@ -239,7 +239,7 @@ function reckoningTick(){
 function processCombinedSession(){
 	curSessionGlobalVar = curSessionGlobalVar.initializeCombinedSession();
 	if(curSessionGlobalVar){
-		$("#story").append("<br><Br> But things aren't over, yet. The survivors manage to contact the players in the universe they created. Time has no meaning between universes, and they are given ample time to plan an escape from their own Game Over. They will travel to the new universe, and register as players there for session " + curSessionGlobalVar.session_id + ". ");
+		$("#story").append("<br><Br> But things aren't over, yet. The survivors manage to contact the players in the universe they created. Their sick frog may have screwed them over, but the connection it provides to their child universe will equally prove to be their salvation. Time has no meaning between universes, and they are given ample time to plan an escape from their own Game Over. They will travel to the new universe, and register as players there for session " + curSessionGlobalVar.session_id + ". ");
 		checkSGRUB();
 		load(curSessionGlobalVar.players, curSessionGlobalVar.guardians); //in loading.js
 	}
