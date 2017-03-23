dfcc6fd676a4f94e7a0ff72760de1dfdfbeefe73
Jenny <gtg287y@gmail.com>
2017-03-23 11:35:00 -0400

    art bg

--- javascripts/art_newsposts.js
+++ javascripts/art_newsposts.js
@@ -5,9 +5,9 @@ window.onload = function() {
 }
 
 function newsposts(){
+	writeNewspost("3/23/17", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ")
 	writeNewspost("3/23/17", "This is just a test")
-	writeNewspost("3/23/17", "This is just a test")
-	writeNewspost("3/23/17", "This is just a test")
+	writeNewspost("3/23/17", "This is just a test. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ")
 	writeNewspost("3/23/17", "This is just a test")
 	writeNewspost("3/23/17", "This is just a test")
 
