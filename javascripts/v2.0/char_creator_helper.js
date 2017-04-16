da797d3737aedbcf728f01cbd45d2a5e7abf4576
Jenny <gtg287y@gmail.com>
2017-04-16 14:17:24 -0400

    unhooked up drop downs

--- javascripts/v2.0/char_creator_helper.js
+++ javascripts/v2.0/char_creator_helper.js
@@ -11,11 +11,52 @@ function CharacterCreatorHelper(players){
 		}
 	}
 	this.drawSinglePlayer = function(player){
+		var str = "";
 		var divId =  player.title()+player.chatHandle;
-		var canvasHTML = "<canvas id='canvas" +divId + "' width='" +400 + "' height="+300 + "'>  </canvas>";
-		this.div.append(canvasHTML);
+		str += "<div class='createdCharacter'>"
+		var canvasHTML = "<canvas class = 'createdCharacterCanvas' id='canvas" +divId + "' width='" +400 + "' height="+300 + "'>  </canvas>";
+		str += "<div class = 'stats'>"
+		str += (this.drawOneClassDropDown(player));
+		str += ("of");
+		str+= (this.drawOneAspectDropDown(player));
+		str += "</div>"
+
+		str += (canvasHTML);
+		str += "</div>"
+		this.div.append(str);
 		var canvas = document.getElementById("canvas"+ divId);
 		drawSinglePlayer(canvas, player);
+
+	}
+
+	this.drawOneClassDropDown = function(player){
+		available_classes = classes.slice(0); //re-init available classes. make deep copy
+		var html = "<select name='className" +player.chatHandle +"'>";
+		for(var i = 0; i< available_classes.length; i++){
+			if(available_classes[i] == player.class_name){
+				html += '<option  selected = "selected" value="' + available_classes[i] +'">' + available_classes[i]+'</option>'
+			}else{
+				html += '<option value="' + available_classes[i] +'">' + available_classes[i]+'</option>'
+			}
+		}
+		html += '</select>'
+		return html;
+	}
+
+	this.drawOneAspectDropDown = function(player){
+		available_aspects = nonrequired_aspects.slice(0); //required_aspects
+	  available_aspects = available_aspects.concat(required_aspects.slice(0));
+		var html = "<select name='aspect" +player.chatHandle +"'>";
+		for(var i = 0; i< available_aspects.length; i++){
+			if(available_aspects[i] == player.aspect){
+				html += '<option selected = "selected" value="' + available_aspects[i] + '" >' + available_aspects[i]+'</option>'
+			}else{
+				html += '<option value="' + available_aspects[i] + '" >' + available_aspects[i]+'</option>'
+			}
+
+		}
+		html += '</select>'
+		return html;
 	}
 
 }
