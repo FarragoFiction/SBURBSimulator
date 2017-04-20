d30b2b28d366e599ccf05449952a263b349f955a
JR <jennifer.schultz@ericsson.com>
2017-04-20 09:23:31 -0400

    luck scenes should be done besides testing

--- javascripts/Scenes/LuckStuff.js
+++ javascripts/Scenes/LuckStuff.js
@@ -40,7 +40,15 @@ function LuckStuff(session){
 		roll.player.increasePower();
 		return ret;
 	}
+	//decreasing power is not a thing. so only land level?
+	this.roll40 = function(roll){
+		console.log("roll40 in " + this.session.session_id)
+		var ret = "The " + roll.player.htmlTitleBasic() + " was just wandering around on " + roll.player.shortLand()+ " when they suddenly tripped over a huge bee hive. The angry bees immediately ravage the country side, pestering local consorts.";
+		roll.player.landLevel += -1
+		return ret;
+	}
 	
+
 	this.roll70 = function(roll){
 		console.log("roll70 in " + this.session.session_id)
 		var friend = randomElementFromArray(roll.player.getFriendsFromList());
@@ -49,6 +57,17 @@ function LuckStuff(session){
 		}
 		var ret = "The " + roll.player.htmlTitleBasic() + " was fucking around on the internet and accidentally sent the " + friend.htmlTitleBasic() + " a message meant for someone else. Luckily, they seem flattered instead of offended. ";
 		player.getRelationshipWith(friend).increase();
+		return ret;
+	}
+	
+	this.roll30(){
+		console.log("roll30 in " + this.session.session_id)
+		var friend = randomElementFromArray(roll.player.getFriendsFromList());
+		if(!friend){
+			return this.roll65(roll); //backup result.
+		}
+		var ret = "The " + roll.player.htmlTitleBasic() + " was fucking around on the internet and accidentally sent the " + friend.htmlTitleBasic() + " a message meant for someone else. They will never live this down. The " + friend.htmlTitleBasic() + " seems pretty offended, too.";
+		player.getRelationshipWith(friend).decrease();
 		return ret;
 	}
 	
@@ -70,6 +89,14 @@ function LuckStuff(session){
 		roll.player.increasePower();
 		this.leader.leveledTheHellUp = true;
 		this.leader.level_index +=2;
+		return ret;
+	}
+	
+	this.roll20 = function(roll){
+		console.log("roll20 in " + this.session.session_id)
+
+		var ret = "The " + roll.player.htmlTitleBasic() + " tripped right through a glitched section of wall, only to find a single consort. 'Shh.' the imp says, handing over a frankly obscene bucket of...something, 'It's a secret to everybody.' The " + roll.player.htmlTitleBasic() + " agrees that it would be ideal if it was a secret even to themselves, and prays for amnesia.  They can't quite bring themselves to go near their consorts for a little while aftewards. " ;
+		roll.player.landLevel += -2
 		return ret;
 	}
 	
@@ -100,6 +127,16 @@ function LuckStuff(session){
 		return ret;
 	}
 	
+	this.roll10 = function(roll){
+		console.log("roll10 in " + this.session.session_id)
+		var ret = "Through a frankly preposterous level of Scooby-Doo shenanigans, the  " + roll.player.htmlTitleBasic() + " trips into a wall, which depresses a panel, which launches a flaming rock via catapult, which crashes into a local consort village. Which immediately catches on fire, which makes them be refugees, which makes them immegrate to a new area, which disrupts the stability of the entire goddamned planet.  All of which causes, like, a third of the main quest of "  + roll.player.shortLand() + " to be fucked up. ";
+		if(roll.player.aspect == "Space"){
+			ret += "Wait. What the HELL!? Is that last Frog!? Just sitting there? Right in front of the " + roll.player.htmlTitleBasic() + "No time shenanigans or prophecies or god damned Choices!? It's just...there. Well. Damn. That'll make the frog breeding WAY easier.";
+		}
+		roll.player.landLevel += -4;		
+		return ret;
+	}
+	
 	this.roll100 = function(roll){
 		console.log("roll100 in " + this.session.session_id);
 		if(roll.player.godDestiny && !roll.player.godTier){
@@ -117,6 +154,13 @@ function LuckStuff(session){
 		
 	}
 	
+	this.roll0 = function(roll){
+		console.log("roll0 in " + this.session.session_id);
+		var ret = "What the HELL!? The " + roll.player.htmlTitleBasic() + " managed to somehow lose to REGULAR FUCKING ENEMIES!? Is that even POSSIBLE!? This is BULLSHIT. How unlucky do you even need to BE!?"
+		return ret;
+		player.dead = true;
+	}
+	
 	
 	
 	this.processRoll(roll){
@@ -130,6 +174,16 @@ function LuckStuff(session){
 			return this.roll80(roll);
 		}else if(roll.value >= 85 && roll.value < 90){
 			return this.roll85(roll);
+		}else if(roll.value > 30 && roll.value <= 40){
+			return this.roll40(roll);
+		}else if(roll.value > 20 && roll.value <= 30){
+			return this.roll30(roll);
+		}else if(roll.value > 10 && roll.value <= 20){
+			return this.roll20(roll);
+		}else if(roll.value > 0 && roll.value <= 10){
+			return this.roll10(roll);
+		}else if(roll.value > 10 && roll.value <= 0){
+			return this.roll0(roll);
 		}
 
 
