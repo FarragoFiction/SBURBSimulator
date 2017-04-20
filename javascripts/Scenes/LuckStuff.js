6a0fc733e7cacb82c16a376d0e3ea1525e75f2d4
JR <jennifer.schultz@ericsson.com>
2017-04-20 12:19:01 -0400

    yeah, can't exactly do luck related land quests if you don't have a land, combo players

--- javascripts/Scenes/LuckStuff.js
+++ javascripts/Scenes/LuckStuff.js
@@ -27,7 +27,7 @@ function LuckStuff(session){
 	
 	this.roll60 = function(roll){
 		//console.log("roll60 in " + this.session.session_id)
-		if(roll.player.aspect != "Space" && roll.player.landLevel >= this.landLevelNeeded){  //not lucky to get land level when you don't need it.
+		if(!roll.player.land || (roll.player.aspect != "Space" && roll.player.landLevel >= this.landLevelNeeded)){  //not lucky to get land level when you don't need it.
 			return this.roll65(roll);
 		}
 		var ret = "The " + roll.player.htmlTitle() + " was just wandering around on " + roll.player.shortLand()+ " when they suddenly tripped over a huge treasure chest! When opened, it revealed a modest hoarde of grist. It will be easier to complete their land quests now.";
@@ -38,13 +38,20 @@ function LuckStuff(session){
 	
 	this.roll65 = function(roll){
 		//console.log("roll65 in " + this.session.session_id)
-		var ret = "The " + roll.player.htmlTitle() + " was just wandering around on " + roll.player.shortLand() + " when they suddenly tripped over a huge treasure chest! When opened, it revealed a modest cache of boonbucks. They will finally be able to afford that framotiff they have had their eye on!";
+		var land = "some random planet"
+		if(roll.player.land){
+			land = roll.player.shortLand();
+		}
+		var ret = "The " + roll.player.htmlTitle() + " was just wandering around on " + land + " when they suddenly tripped over a huge treasure chest! When opened, it revealed a modest cache of boonbucks. They will finally be able to afford that framotiff they have had their eye on!";
 		roll.player.increasePower();
 		this.session.goodLuckEvent = true;
 		return ret;
 	}
 	//decreasing power is not a thing. so only land level?
 	this.roll40 = function(roll){
+		if(!roll.player.land){
+			return; //you've had enough bad luck. just...go rest or something.
+		}
 		//console.log("roll40 in " + this.session.session_id)
 		var ret = "The " + roll.player.htmlTitle() + " was just wandering around on " + roll.player.shortLand()+ " when they suddenly tripped over a huge bee hive. The angry bees immediately ravage the country side, pestering local consorts.";
 		roll.player.landLevel += -1
@@ -83,7 +90,7 @@ function LuckStuff(session){
 	
 	this.roll80 = function(roll){
 		//console.log("roll80 in " + this.session.session_id)
-		if(roll.player.aspect != "Space" && roll.player.landLevel >= this.landLevelNeeded){  //not lucky to get land level when you don't need it.
+		if(!roll.player.land || (roll.player.aspect != "Space" && roll.player.landLevel >= this.landLevelNeeded)){  //not lucky to get land level when you don't need it.
 			return this.roll85(roll);
 		}
 		var ret = "The " + roll.player.htmlTitle() + " tripped right through a glitched section of wall, only to find a single imp. 'Shh.' the imp says, handing over a frankly obscene bucket of grist, 'It's a secret to everybody.' The " + roll.player.htmlTitle() + " agrees that it would be ideal if it was a secret even to themselves, and prays for amnesia.  Like hell are they gonna leave behind the grist, though. Land quests don't solve themselves. " ;
@@ -95,7 +102,11 @@ function LuckStuff(session){
 	
 	this.roll85 = function(roll){
 		//console.log("roll85 in " + this.session.session_id)
-		var ret = "The " + roll.player.htmlTitle() + " was just wandering around on " + roll.player.shortLand() + " they see a GOLD IMP. Those things are worth a ton of experience points, if you can manage to even damage them. Holy shit, did the " + roll.player.htmlTitle() + " just ONE SHOT them!? ";
+		var land = "some random planet"
+		if(roll.player.land){
+			land = roll.player.shortLand();
+		}
+		var ret = "The " + roll.player.htmlTitle() + " was just wandering around on " + land + " they see a GOLD IMP. Those things are worth a ton of experience points, if you can manage to even damage them. Holy shit, did the " + roll.player.htmlTitle() + " just ONE SHOT them!? ";
 		roll.player.increasePower();
 		roll.player.increasePower();
 		roll.player.leveledTheHellUp = true;
@@ -106,7 +117,9 @@ function LuckStuff(session){
 	
 	this.roll20 = function(roll){
 		//console.log("roll20 in " + this.session.session_id)
-
+		if(!roll.player.land){
+			return; //you've had enough bad luck. just...go rest or something.
+		}
 		var ret = "The " + roll.player.htmlTitle() + " tripped right through a glitched section of wall, only to find a single consort. 'Shh.' the imp says, handing over a frankly obscene bucket of...something, 'It's a secret to everybody.' The " + roll.player.htmlTitleBasic() + " agrees that it would be ideal if it was a secret even to themselves, and prays for amnesia.  They can't quite bring themselves to go near their consorts for a little while aftewards. " ;
 		roll.player.landLevel += -2
 		this.session.badLuckEvent = true;
@@ -127,7 +140,7 @@ function LuckStuff(session){
 	
 	this.roll95 = function(roll){
 		//console.log("roll95 in " + this.session.session_id)
-		if(roll.player.aspect != "Space" && roll.player.landLevel >= this.landLevelNeeded){  //not lucky to get land level when you don't need it.
+		if(!roll.player.land || (roll.player.aspect != "Space" && roll.player.landLevel >= this.landLevelNeeded)){  //not lucky to get land level when you don't need it.
 			return this.roll90(roll);
 		}
 		var ret = "Through a frankly preposterous level of Scooby-Doo shenanigans, the  " + roll.player.htmlTitle() + " trips into a wall, which depresses a panel, which launches a catapult, which throws impudent fruit at a nearby Ogre, which wakes him up, which makes him wander away, which frees the local consorts from his tyranny, who then celebrate an end to their famine by eating the fruit.  All of which causes, like, a third of the main quest of "  + roll.player.shortLand() + " to be completed. ";
@@ -144,6 +157,9 @@ function LuckStuff(session){
 	
 	this.roll10 = function(roll){
 		//console.log("roll10 in " + this.session.session_id)
+		if(!roll.player.land){
+			return; //you've had enough bad luck. just...go rest or something.
+		}
 		var ret = "Through a frankly preposterous level of Scooby-Doo shenanigans, the  " + roll.player.htmlTitle() + " trips into a wall, which depresses a panel, which launches a flaming rock via catapult, which crashes into a local consort village. Which immediately catches on fire, which makes them be refugees, which makes them immegrate to a new area, which disrupts the stability of the entire goddamned planet.  All of which causes, like, a third of the main quest of "  + roll.player.shortLand() + " to be fucked up. ";
 		roll.player.landLevel += -4;
 		this.session.badLuckEvent = true;
@@ -152,7 +168,8 @@ function LuckStuff(session){
 	
 	this.roll100 = function(roll){
 		//console.log("roll100 in " + this.session.session_id + " roll is: " + roll.value);
-		if(roll.player.godDestiny && !roll.player.godTier){
+		
+		if(roll.player.godDestiny && !roll.player.godTier && roll.player.dreamSelf){
 			var ret = "What the HELL!? The " + roll.player.htmlTitle() + " managed to somehow lose to REGULAR FUCKING ENEMIES!? Is that even POSSIBLE!? This is BULLSHIT. Wait. What's going on? How did they end up on their " ;
 			if(!roll.player.isDreamSelf){
 				ret += "QUEST BED!? Their body glows, and rises Skaiaward. "+"On " + roll.player.moon + ", their dream self takes over and gets a sweet new outfit to boot.  ";
