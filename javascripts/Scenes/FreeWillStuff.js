63607dac65b551d61b71cebe53b414e1d6bac0b6
JR <jennifer.schultz@ericsson.com>
2017-04-20 14:37:35 -0400

    notes

--- javascripts/Scenes/FreeWillStuff.js
+++ javascripts/Scenes/FreeWillStuff.js
@@ -23,13 +23,30 @@ function FreeWillStuff(session){
 		div.append("<br>"+this.content());
 	}
 	
+	//in murder mode, plus random. reduce trigger, too.
 	this.considerDisEngagingMurderMode = function(player){
 		return null;
 	}
 	
+	//hate someone, not in murder mode, plus random, increase trigger, too.
 	this.considerEngagingMurderMode = function(player){
 		
 		return null;
+	}
+	
+	// do it to self if active, do it to someone else if not.  need to have it not be destiny. bonus if there are dead players (want to avenge them/stop more corpses).
+	this.considerForceGodTier = function(player){
+		
+	}
+	
+	//needs to be a murder mode player. more likely if you like them.  if active and you like them a lot, do it yourself. if passive, see if you can get somebody else to do it for you (mastermind)
+	this.considerCalmMurderModePlayer = function(player){
+		
+	}
+	
+	//needs to be a murdermode player,  more likely if you dislike them. if active, do it yourself, if passive, see if you can get somebody else to do it for you. need to be stronger than them. 
+	this.considerKillMurderModePlayer = function(player){
+		
 	}
 	
 	
@@ -40,12 +57,14 @@ function FreeWillStuff(session){
 	}
 	
 	
+	
+	
 	this.processDecision = function(){
 		
 	}
 	
 	this.content = function(){
-		var ret = "Luck Event: ";
+		var ret = "Decision Event: ";
 		removeFromArray(this.player, this.session.availablePlayers);
 		for(var i = 0; i<this.wills.length; i++){
 			var will = this.wills[i];
