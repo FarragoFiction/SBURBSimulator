48a592eec8e2a870f003721af041ebe2f4aa4b93
Jenny <gtg287y@gmail.com>
2017-04-30 10:43:33 -0400

    stubbing out boss fight rules

--- javascripts/gameEntity.js
+++ javascripts/gameEntity.js
@@ -8,21 +8,79 @@
 function GameEntity(session, name){
 		this.session = session;
 		this.name = name;
+		//if any stat is -1025, it's considered to be infinitie. denizens use. you can't outluck Cetus, she is simply the best there is.
 		this.minLuck = 0;
+		this.hp = 0;  //what does infinite hp mean? you need to defeat them some other way. alternate win conditions? or can you only do The Choice?
+		this.mobility = 0;
 		this.maxLuck = 0;
+		this.freeWill = 0; //jack has extremely high free will. why he is such a wild card
 		this.relationships = [];
 		this.power = 0;
 		this.dead = false;
-		this.landLevel  = 100; //use this instead of hp so thief of life can steal from you?
+		this.escapeable = true;
+		this.canFlee = true;
+
+		//each prototype increases base power. some prototypes also modify other stats. have arrays just like fortune/disastor prototypings.
+		//if it's a player...copy all player stats?
+		this.addPrototype = function(object){
+
+		}
+
+		//if jack gets the queen rings, her stats are added onto his.
+		this.addStatsFromObject = function(object){
+
+		}
+
+		//a player will try to flee this fight if they are losing.
+		//but if any of their good friends are still around, they will stay.
+		//if all players are fled, fight is over.
+		//some fights you can't run from. king/queen as example.
+		//mobility needs to be high enough. mention if you try to flee and get cut off.
+		this.willPlayerFlee = function(player){
+
+		}
+
+		//denizen and king/queen will never flee. but jack and planned mini bosses can.
+		//flee if you are losing. mobility needs to be high enough. mention if you try to flee and get cut off.
+		this.willIFlee= function(){
+
+		}
+
+
+		//before a fight is called, decide who is in it. denizens are one on one, jack catches slower player and friends
+		//king/queen are whole party. if you want to comment on who's in it, do it before here.
+		//time clones count as players. have "doomed" in their title. that means players have a "doomed" stat.
+		//target doomed players preferientially, even over any other algorithm.
+		/*
+		players can fight, flee (if available) or special. rpg rules, yo.
+		fight uses power directly, flee uses mobility.  special is different for differnt players. if you have ghostPacts you can revive or do a ghostAttack, for example.
+		need to brainstorm special effects.  hope player have hope field if they are last man standing, for example. some players can make zombies of corpses. only in boss fights.
+
+		enemies can fight, flee (if available) or special.  special varies based on enemy.  denizens can do shit like "echolocataclysm", anything prototyped depends on its
+		prototyping. vast glub for horror terror is example.
+		*/
+		this.fight = function(div, players){
+				//a fight is involved. mutiple things happen before this finally returns. keep going until one side is all dead, or fled.
+		}
+
+
+		//some denizens have infinitei HP. what does this mean? how are you supposed to beat them?
+		this.winConditionMet = function(){
+
+		}
+
 
 
 		//place holders for now. being in diamonds with jack is NOT a core feature.
 		this.boostAllRelationshipsWithMeBy = function(amount){
 
 		};
+
 		this.boostAllRelationshipsBy = function(amount){
 
 		};
-		
-		//~~~~~~~~~~~~~~~~~~~~~~~~TODO!!!!!!!!!!!!!!!!!!!!!!!  allow doomed time clones to be treated as "players". if they die, add them to graveyard.
+
+
+
+		//~~~~~~~~~~~~~~~~~~~~~~~~TODO!!!!!!!!!!!!!!!!!!!!!!!  allow doomed time clones to be treated as "players". if they die, add them to afterlife.
 }
