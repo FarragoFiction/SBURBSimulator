60b45afdeb08811e69c61b3c0c3db35cbef92f8c
Jenny <gtg287y@gmail.com>
2017-05-02 17:59:30 -0400

    if jack takes ring that queen has removed, call it a heist

--- javascripts/Scenes/JackPromotion.js
+++ javascripts/Scenes/JackPromotion.js
@@ -19,10 +19,18 @@ function JackPromotion(session){
 		this.session.queen.crowned = null;
 		var ret = " In a shocking turn of events, Jack Noir claims the Black Queen's RING OF ORBS " + this.session.convertPlayerNumberToWords();
 		ret += "FOLD. "
-		if(this.session.queen.getHP() > 0){
-			ret += "He easily defeats the weakened queen and uses her ring to obtain her power. ";
+		if(this.session.queen.crowned){
+			if(this.session.queen.getHP() > 0){
+				ret += "He easily defeats the weakened queen and uses her ring to obtain her power. ";
+			}else{
+				ret += " He pries the ring off her still twitching finger. ";
+			}
 		}else{
-			ret += " He pries the ring off her still twitching finger. ";
+			ret += "It's not hard at all to get his Crew to pull off a heist to get the RING OF ORBS "+ this.session.convertPlayerNumberToWords();
+			ret += "FOLD. "
+			if(this.session.queen.getHP() > 0){
+				ret += "He easily defeats the weakened queen while he's at it. ";
+			}
 		}
 		ret += " You'd think this would be no worse than having the Black Queen around, but Jack is kind of a big deal. ";
 		ret += " He immediately decides to show everybody his stabs. ";
