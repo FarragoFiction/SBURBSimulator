5e2beb804ea4e344c3a4ae47870c9d83824ca26d
JR <jennifer.schultz@ericsson.com>
2017-05-05 12:33:57 -0400

    only heal players if they win their fight, not if they die or run

--- javascripts/gameEntity.js
+++ javascripts/gameEntity.js
@@ -272,7 +272,6 @@ function GameEntity(session, name, crowned){
 			this.fraymotifsUsed = []; //not used yet
 			this.playersAbsconded = [];
 			this.iAbscond = false;
-			this.healPlayers(players);  //they are using items or whatever. don't worry about it.
 		}
 		
 		
@@ -324,6 +323,7 @@ function GameEntity(session, name, crowned){
 				div.append(" <Br><br> The fight is over. " + this.name + " is dead. ");
 				this.levelPlayers(players) //even corpses
 				this.givePlayersGrist(players);
+				this.healPlayers(div, players);//if i heal players after EVERY fight, infinite loops if they run from denizens
 				this.ending(div, players)
 				return true;
 			}//TODO have alternate win conditions for denizens???
