dc54dc524dd83cfb31f3698f18915139b5887e35
Jenny <gtg287y@gmail.com>
2017-05-12 23:42:37 -0400

    trying out mobile css bandaid

--- javascripts/newsposts.js
+++ javascripts/newsposts.js
@@ -142,9 +142,13 @@ function startSession(){
 //if screens are tiny enough, put newposts UNDER avatars instead of next to. but that shouldn't be the default option.
 function reFormatForTinyScreens(){
 		if(screen.width<1000+250){ //guessing here.
-			$("#robo_newsposts").css({top: 600, position:'relative'})
-			$("#newspostsMain").css({top: 600, position:'relative'})
-			$("#artist_newspostsMain").css({top: 600, position:'relative'})
+			$("#robo_newsposts").css({top: 600, position:'relative', width:"450px"})
+			$("#newspostsMain").css({top: 600, position:'relative', width:"450px"})
+			$("#artist_newspostsMain").css({top: 600, position:'relative', width:"450px"})
+		}else{
+			$("#robo_newsposts").css({width:"450px"})
+			$("#newspostsMain").css({width:"450px"})
+			$("#artist_newspostsMain").css({width:"450px"})
 		}
 }
 
