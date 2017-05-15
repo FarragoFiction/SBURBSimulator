f2e7874572313f5e5dfd5f7407d40eddcd81b0f7
Jenny <gtg287y@gmail.com>
2017-05-15 11:47:42 -0400

    shored up cracks where death and godtiering used to be in luck. they are less common now

--- javascripts/Scenes/LuckStuff.js
+++ javascripts/Scenes/LuckStuff.js
@@ -302,7 +302,12 @@ function LuckStuff(session){
 			return this.roll10(roll);
 		}else if(roll.value <= this.minLowValue - (amount*20)){
 			return this.roll0(roll,div);
+		}else if(roll.value < this.minLowValue){ //if I got here, i fell in the crack left where the old death value used to be
+			return this.roll10(roll);  //used to be enough for a roll 0, but not anymore.
+		}else if(roll.value > this. minHighValue){//if i got here, i fell in the crack left where the old godtier value used to be.
+			return this.roll(95);
 		}
+
 		else return "What the hell, mate? roll was: " + roll.value + " and needed to be not between  " + this.minLowValue + " and " + this.minHighValue;
 
 
