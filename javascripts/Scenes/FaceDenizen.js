077799bbd0bedd61b99c706e7e24c8e4a9e1be6f
Jenny <gtg287y@gmail.com>
2017-05-20 20:57:46 -0400

    todo

--- javascripts/Scenes/FaceDenizen.js
+++ javascripts/Scenes/FaceDenizen.js
@@ -99,6 +99,14 @@ function FaceDenizen(session){
 				p.denizenDefeated = true;
 				p.power = p.power*2;  //current and future doubling of power.
 				this.session.denizenBeat = true;
+			}else if(p.dead){
+				var divID = (div.attr("id"))
+				var canvasHTML = "<br><canvas id='canvas" + divID+"' width='" +canvasWidth + "' height="+canvasHeight + "'>  </canvas>";
+				div.append(canvasHTML);
+				var canvas = document.getElementById("canvas"+ divID);
+				drawSinglePlayer(canvas, p);
+				denizenKill(canvas, p);
+				console.log("denizen kill " + this.session.session_id)
 			}
 		}
 			p.denizenFaced = true; //may not have defeated them, but no longer have the option of The Choice
