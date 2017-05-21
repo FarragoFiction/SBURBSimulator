0e33d49b98591e257890d01099c06aa78a605a9c
Jenny <gtg287y@gmail.com>
2017-05-20 22:15:56 -0400

    moon icons

--- javascripts/Scenes/ExploreMoon.js
+++ javascripts/Scenes/ExploreMoon.js
@@ -80,7 +80,9 @@ function ExploreMoon(session){
 	}
 
 	this.renderContent = function(div){
-		div.append("<br>"+this.content());
+		if(this.player1.moon == "Prospit")div.append("<br><img src = 'images/sceneIcons/prospit_icon.png'>")
+		if(this.player1.moon == "Derse")div.append("<br><img src = 'images/sceneIcons/derse_icon.png'>")
+		div.append(this.content());
 	}
 	this.trigger = function(playerList){
 		this.player1 = null; //reset
