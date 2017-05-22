d832e1ec3604e30f5a8b0322ae186e2a83dc61d4
JR <jennifer.schultz@ericsson.com>
2017-05-22 15:25:33 -0400

    undefined quests sometimes

--- javascripts/player.js
+++ javascripts/player.js
@@ -424,7 +424,7 @@ function Player(session,class_name, aspect, object_to_prototype, moon, godDestin
 				return getRandomQuestFromClass(this.class_name);
 			}
 		}else if(this.denizenDefeated){
-			console.log("post denizen quests " +this.session.session_id);
+			//console.log("post denizen quests " +this.session.session_id);
 			//return "restoring their land from the ravages of " + this.session.getDenizenForPlayer(this).name;
 			if(Math.seededRandom() > .5 || this.aspect == "Space"){ //back to having space players be locked to frogs.
 				return getRandomQuestFromAspect(this.aspect,true);
