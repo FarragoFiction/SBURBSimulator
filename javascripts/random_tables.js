7bb0ebbd3bb24450f86e859d23ad10bc6c3ec1df
JR <jennifer.schultz@ericsson.com>
2017-05-25 10:09:55 -0400

    enabled startsWith for internet explorer

--- javascripts/random_tables.js
+++ javascripts/random_tables.js
@@ -1615,3 +1615,11 @@ function helloWorld(){
 	});
 
 }
+
+//IE doesn't support "starts with"
+if (!String.prototype.startsWith) {
+  String.prototype.startsWith = function(searchString, position) {
+    position = position || 0;
+    return this.indexOf(searchString, position) === position;
+  };
+}
