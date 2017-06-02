c4733040cf3605c8d8790b27b3709ba43cc014cd
Jenny <gtg287y@gmail.com>
2017-06-01 21:05:53 -0400

    hof

--- javascripts/Scenes/QuadrantDialogue.js
+++ javascripts/Scenes/QuadrantDialogue.js
@@ -124,7 +124,7 @@ function QuadrantDialogue(session){
 			var chats = [];
 			if(p1.interest1 == "Rap"||p1.interest2 == "Rap" || p1.interest1 == "Turntables" || p1.interest2 == "Turntables"){
 				//pass a 4 so you only get 1 line.
-				chats.push(new InterestConversationalPair(interest, getRapForPlayer(p1, "", 4), ["Yeah dog you got that mad flow.","Shit, your rhymes are tight.","Hell yes. Hell FUCKING yes.","Your beats are hella ill!"], [getRapForPlayer(p2, "", 4)]))
+				chats.push(new InterestConversationalPair(interest, getRapForPlayer(p1, "", 4)[0], ["Yeah dog you got that mad flow.","Shit, your rhymes are tight.","Hell yes. Hell FUCKING yes.","Your beats are hella ill!"], [getRapForPlayer(p2, "", 4)[0]]))
 				return this.processChatAboutInterests(chats, interest, p1,p2,p1Start, p2Start, relationship, relationship2);
 			}
 			//InterestConversationalPair(interest, line1, responseLinesSharedInterestPositive, responseLinesSharedInterestNegative))
