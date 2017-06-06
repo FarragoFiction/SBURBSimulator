803e598775e513e9d84bba301795c1e48fe70963
Jenny <gtg287y@gmail.com>
2017-06-06 18:49:03 -0400

    tweaking murdermode and grim dark rates, probably

--- javascripts/Scenes/GoGrimDark.js
+++ javascripts/Scenes/GoGrimDark.js
@@ -10,12 +10,16 @@ function GoGrimDark(session){
 		//select a random player. if they've been triggered, random chance of going grim dark (based on how triggered.)
 		for(var i = 0; i< this.playerList.length; i++){//never, EVER fix this to be available players.  corpses shouldu ALWAYS be able to go grim dark, this is HILARIOUS
 			var p = this.playerList[i]
-			if(p.corruptionLevelOther >= 50){
+			if(this.corruptionReachedTippingPoint(p)){
 				this.player = p;
 				return true;
 			}
 		}
 		return false;
+	}
+
+	this.corruptionReachedTippingPoint = function(p){
+		return p.corruptionLevelOther >= 50-(p.grimDark*10) //at level 4, only need 10 more points. at level 1, need 40
 	}
 
 	this.addImportantEvent = function(){
@@ -30,7 +34,6 @@ function GoGrimDark(session){
 		}
 		div.append("<br><img src = 'images/sceneIcons/grimdark_black_icon.png'>"+this.content());
 		if(this.player.grimDark ==4){
-			console.log("full grim dark in: " + this.session.session_id)
 			var divID = div.attr("id") + "grimdark"
 			var canvasHTML = "<br><canvas id='canvas" + divID+"' width='" +canvasWidth + "' height="+canvasHeight + "'>  </canvas>";
 			div.append(canvasHTML);
@@ -57,7 +60,7 @@ function GoGrimDark(session){
 			}else if(this.player.grimDark == 4){
 				this.player.power += 100;
 				console.log("full grim dark: " + this.session.session_id)
-				this.player.aspectIncreasePower(100);
+				this.player.aspectIncreasePower(200);
 				ret +=  "The " + this.player.htmlTitleBasic() + " slips into the fabled blackdeath trance of the woegothics, quaking all the while in the bloodeldritch throes of the broodfester tongues.";
 				ret += " It is now painfully obvious to anyone with a brain, they have basically gone completely off the deep end in every way. The "
 				ret += this.player.htmlTitle() + " has officially gone grimdark. ";
