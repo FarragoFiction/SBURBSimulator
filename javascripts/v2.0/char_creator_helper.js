fbd5ad62fde047d05307d19434517456a4e8cbe2
JR <jennifer.schultz@ericsson.com>
2017-06-07 15:08:03 -0400

    why are bools always false???

--- javascripts/v2.0/char_creator_helper.js
+++ javascripts/v2.0/char_creator_helper.js
@@ -14,8 +14,8 @@ function CharacterCreatorHelper(players){
 	this.drawSinglePlayer = function(player){
 		//console.log("drawing: " + player.title())
 		var str = "";
-		var divId =  player.chatHandle;
-		divId = divId.replace(/\s+/g, '')
+		var divId =  player.id;
+		//divId = divId.replace(/\s+/g, '')
 		str += "<div class='createdCharacter'>"
 		var canvasHTML = "<canvas class = 'createdCharacterCanvas' id='canvas" +divId + "' width='" +400 + "' height="+300 + "'>  </canvas>";
 		str += "<div class = 'charOptions'>"
@@ -36,7 +36,7 @@ function CharacterCreatorHelper(players){
 		str += "</div>"
 		this.div.append(str);
 
-		player.spriteCanvasID = player.chatHandle+player.id+"spriteCanvas";
+		player.spriteCanvasID = player.id+player.id+"spriteCanvas";
 		var canvasHTML = "<br><canvas style='display:none' id='" + player.spriteCanvasID+"' width='" +400 + "' height="+300 + "'>  </canvas>";
 		$("#playerSprites").append(canvasHTML)
 
@@ -55,7 +55,7 @@ function CharacterCreatorHelper(players){
 
 	this.redrawSinglePlayer = function(player){
 		  player.renderSelf();
-			var divId = "canvas" + player.chatHandle;
+			var divId = "canvas" + player.id;
 			divId = divId.replace(/\s+/g, '');
 			var canvas =$("#"+divId)[0]
 			drawSolidBG(canvas, "#ffffff")
@@ -64,69 +64,69 @@ function CharacterCreatorHelper(players){
 
 
 	this.wireUpPlayerDropDowns = function(player){
-			var c2 =  $("#classNameID" +player.chatHandle) ;
-			var a2 =  $("#aspectID" +player.chatHandle) ;
-			var hairDiv  =  $("#hairTypeID" +player.chatHandle) ;
-			var hairColorDiv  =  $("#hairColorID" +player.chatHandle) ;
-			var speciesDiv  =  $("#speciesID" +player.chatHandle) ;
-			var leftHornDiv  =  $("#leftHornID" +player.chatHandle) ;
-			var rightHornDiv  =  $("#rightHornID" +player.chatHandle) ;
-			var bloodDiv  =  $("#bloodColorID" +player.chatHandle) ;
-			var favoriteNumberDiv  =  $("#favoriteNumberID" +player.chatHandle) ;
+			var c2 =  $("#classNameID" +player.id) ;
+			var a2 =  $("#aspectID" +player.id) ;
+			var hairDiv  =  $("#hairTypeID" +player.id) ;
+			var hairColorDiv  =  $("#hairColorID" +player.id) ;
+			var speciesDiv  =  $("#speciesID" +player.id) ;
+			var leftHornDiv  =  $("#leftHornID" +player.id) ;
+			var rightHornDiv  =  $("#rightHornID" +player.id) ;
+			var bloodDiv  =  $("#bloodColorID" +player.id) ;
+			var favoriteNumberDiv  =  $("#favoriteNumberID" +player.id) ;
 
 
 			var that = this;
 			c2.change(function() {
-					var classDropDown = $('[name="className' +player.chatHandle +'"] option:selected') //need to get what is selected inside the .change, otheriise is always the same
+					var classDropDown = $('[name="className' +player.id +'"] option:selected') //need to get what is selected inside the .change, otheriise is always the same
 					player.class_name = classDropDown.val();
 					that.redrawSinglePlayer(player);
 			});
 
 			favoriteNumberDiv.change(function() {
-					var numberDropDown = $('[name="favoriteNumber' +player.chatHandle +'"] option:selected') //need to get what is selected inside the .change, otheriise is always the same
+					var numberDropDown = $('[name="favoriteNumber' +player.id +'"] option:selected') //need to get what is selected inside the .change, otheriise is always the same
 					player.quirk.favoriteNumber = numberDropDown.val();
 					that.redrawSinglePlayer(player);
 			});
 
 
 			a2.change(function() {
-					var aspectDropDown = $('[name="aspect' +player.chatHandle +'"] option:selected')
+					var aspectDropDown = $('[name="aspect' +player.id +'"] option:selected')
 					player.aspect = aspectDropDown.val();
 					that.redrawSinglePlayer(player);
 			});
 
 			hairDiv.change(function() {
-				  var aspectDropDown = $('[name="hair' +player.chatHandle +'"] option:selected')
+				  var aspectDropDown = $('[name="hair' +player.id +'"] option:selected')
 					player.hair = aspectDropDown.val();
 					that.redrawSinglePlayer(player);
 			});
 
 			hairColorDiv.change(function() {
-					//var aspectDropDown = $('[name="hairColor' +player.chatHandle +'"] option:selected')
+					//var aspectDropDown = $('[name="hairColor' +player.id +'"] option:selected')
 					player.hairColor = hairColorDiv.val();
 					that.redrawSinglePlayer(player);
 			});
 
 			leftHornDiv.change(function() {
-					var aspectDropDown = $('[name="leftHorn' +player.chatHandle +'"] option:selected')
+					var aspectDropDown = $('[name="leftHorn' +player.id +'"] option:selected')
 					player.leftHorn = aspectDropDown.val();
 					that.redrawSinglePlayer(player);
 			});
 
 			rightHornDiv.change(function() {
-					var aspectDropDown = $('[name="rightHorn' +player.chatHandle +'"] option:selected')
+					var aspectDropDown = $('[name="rightHorn' +player.id +'"] option:selected')
 					player.rightHorn = aspectDropDown.val();
 					that.redrawSinglePlayer(player);
 			});
 
 			bloodDiv.change(function() {
-					var aspectDropDown = $('[name="bloodColor' +player.chatHandle +'"] option:selected')
+					var aspectDropDown = $('[name="bloodColor' +player.id +'"] option:selected')
 					player.bloodColor = aspectDropDown.val();
 					that.redrawSinglePlayer(player);
 			});
 
 			speciesDiv.change(function() {
-					var aspectDropDown = $('[name="species' +player.chatHandle +'"] option:selected')
+					var aspectDropDown = $('[name="species' +player.id +'"] option:selected')
 					var str = aspectDropDown.val();
 					if(str == "Troll"){
 						player.isTroll = true;
@@ -139,30 +139,30 @@ function CharacterCreatorHelper(players){
 	
 	this.wireUpInterests = function(player){
 		//first, choosing interest category should change the contents of interestDrop1 or 2 (but NOT any value in the player or the text box.)
-		var interestCategory1Dom =  $("#interestCategory1" +player.chatHandle);
-		var interestCategory2Dom =  $("#interestCategory2" +player.chatHandle);
-		var interest1DropDom =  $("#interestDrop1" +player.chatHandle);
-		var interest2DropDom =  $("#interestDrop2" +player.chatHandle);
-		var interest1TextDom =  $("#interest1" +player.chatHandle); //don't wire these up. instead, get value on url creation.
-		var interest2TextDom =  $("#interest2" +player.chatHandle);
+		var interestCategory1Dom =  $("#interestCategory1" +player.id);
+		var interestCategory2Dom =  $("#interestCategory2" +player.id);
+		var interest1DropDom =  $("#interestDrop1" +player.id);
+		var interest2DropDom =  $("#interestDrop2" +player.id);
+		var interest1TextDom =  $("#interest1" +player.id); //don't wire these up. instead, get value on url creation.
+		var interest2TextDom =  $("#interest2" +player.id);
 		var that = this;
 		interestCategory1Dom.change(function() {
-					var icDropDown = $('[name="interestCategory1' +player.chatHandle +'"] option:selected')
+					var icDropDown = $('[name="interestCategory1' +player.id +'"] option:selected')
 					interest1DropDom.html(that.drawInterestDropDown(icDropDown.val(), 1, player))
 		});
 		
 		interestCategory2Dom.change(function() {
-					var icDropDown = $('[name="interestCategory2' +player.chatHandle +'"] option:selected')
+					var icDropDown = $('[name="interestCategory2' +player.id +'"] option:selected')
 					interest2DropDom.html(that.drawInterestDropDown(icDropDown.val(), 2, player))
 		});
 		
 		interest1DropDom.change(function() {
-					var icDropDown = $('[name="interestDrop1' +player.chatHandle +'"] option:selected')
+					var icDropDown = $('[name="interestDrop1' +player.id +'"] option:selected')
 					interest1TextDom.val(icDropDown.val());
 		});
 		
 		interest2DropDom.change(function() {
-					var icDropDown = $('[name="interestDrop2' +player.chatHandle +'"] option:selected')
+					var icDropDown = $('[name="interestDrop2' +player.id +'"] option:selected')
 					interest2TextDom.val(icDropDown.val());
 		});
 		
@@ -170,7 +170,7 @@ function CharacterCreatorHelper(players){
 
 	//(1,60)
 	this.drawOneHairDropDown = function(player){
-		var html = "<select id = 'hairTypeID" + player.chatHandle + "' name='hair" +player.chatHandle +"'>";
+		var html = "<select id = 'hairTypeID" + player.id + "' name='hair" +player.id +"'>";
 		for(var i = 1; i<= 60; i++){
 			if(player.hair == i){
 				html += '<option  selected = "selected" value="' + i +'">' + i+'</option>'
@@ -196,12 +196,12 @@ function CharacterCreatorHelper(players){
 	this.drawInterestTextBox = function(num,player){
 		var interestToCheck = player.interest1;
 		if(num == 2) interestToCheck = player.interest2;
-		var html = "<input type='text' id = 'interest" + num+ player.chatHandle + "' name='interest" +num+player.chatHandle +"' + value='" + interestToCheck +"'> </input>"
+		var html = "<input type='text' id = 'interest" + num+ player.id + "' name='interest" +num+player.id +"' + value='" + interestToCheck +"'> </input>"
 		return html;
 	}
 	
 	this.drawInterestDropDown = function(category, num, player){
-		var html = "<select id = 'interestDrop" + num+ player.chatHandle + "' name='interestDrop" +num+player.chatHandle +"'>";
+		var html = "<select id = 'interestDrop" + num+ player.id + "' name='interestDrop" +num+player.id +"'>";
 		var interestsInCategory = interestCategoryToInterestList(category);
 		var interestToCheck = player.interest1;
 		if(num == 2) interestToCheck = player.interest2;
@@ -218,7 +218,7 @@ function CharacterCreatorHelper(players){
 	}
 	
 	this.drawInterestCategoryDropDown = function(num,player){
-		var html = "<select id = 'interestCategory" + num+ player.chatHandle + "' name='interestCategory" +num+player.chatHandle +"'>";
+		var html = "<select id = 'interestCategory" + num+ player.id + "' name='interestCategory" +num+player.id +"'>";
 		for(var i = 0; i< interestCategories.length; i++){
 			var ic = interestCategories[i];
 			if(player.interestedIn(ic, num)){
@@ -238,7 +238,7 @@ function CharacterCreatorHelper(players){
 
 	//0,12
 	this.drawOneFavoriteNumberDropDown = function(player){
-		var html = "<select id = 'favoriteNumberID" + player.chatHandle + "' name='favoriteNumber" +player.chatHandle +"'>";
+		var html = "<select id = 'favoriteNumberID" + player.id + "' name='favoriteNumber" +player.id +"'>";
 		for(var i = 0; i<= 12; i++){
 			if(player.quirk.favoriteNumber == i){
 				html += '<option  selected = "selected" value="' + i +'">' + i+'</option>'
@@ -251,7 +251,7 @@ function CharacterCreatorHelper(players){
 	}
 
 	this.drawOneLeftHornDropDown = function(player){
-		var html = "<select id = 'leftHornID" + player.chatHandle + "' name='leftHorn" +player.chatHandle +"'>";
+		var html = "<select id = 'leftHornID" + player.id + "' name='leftHorn" +player.id +"'>";
 		for(var i = 1; i<= 46; i++){
 			if(player.leftHorn == i){
 				html += '<option  selected = "selected" value="' + i +'">' + i+'</option>'
@@ -264,7 +264,7 @@ function CharacterCreatorHelper(players){
 	}
 
 	this.drawOneRightHornDropDown = function(player){
-		var html = "<select id = 'rightHornID" + player.chatHandle + "' name='rightHorn" +player.chatHandle +"'>";
+		var html = "<select id = 'rightHornID" + player.id + "' name='rightHorn" +player.id +"'>";
 		for(var i = 1; i<= 46; i++){
 			if(player.leftHorn == i){
 				html += '<option  selected = "selected" value="' + i +'">' + i+'</option>'
@@ -278,7 +278,7 @@ function CharacterCreatorHelper(players){
 
 	this.drawOneClassDropDown = function(player){
 		available_classes = classes.slice(0); //re-init available classes. make deep copy
-		var html = "<select id = 'classNameID" + player.chatHandle + "' name='className" +player.chatHandle +"'>";
+		var html = "<select id = 'classNameID" + player.id + "' name='className" +player.id +"'>";
 		for(var i = 0; i< available_classes.length; i++){
 			if(available_classes[i] == player.class_name){
 				html += '<option  selected = "selected" value="' + available_classes[i] +'">' + available_classes[i]+'</option>'
@@ -292,7 +292,7 @@ function CharacterCreatorHelper(players){
 
 	this.drawOneSpeciesDropDown = function(player){
 		var species = ["Human", "Troll"]
-		var html = "<select id = 'speciesID" + player.chatHandle + "' name='species" +player.chatHandle +"'>";
+		var html = "<select id = 'speciesID" + player.id + "' name='species" +player.id +"'>";
 		for(var i = 0; i< species.length; i++){
 			if((species[i] == "Troll" && player.isTroll) || (species[i] == "Human" && !player.isTroll)){
 				html += '<option  selected = "species" value="' + species[i] +'">' + species[i]+'</option>'
@@ -306,12 +306,12 @@ function CharacterCreatorHelper(players){
 	}
 
 	this.drawOneHairColorPicker = function(player){
-		var id = "hairColorID" + player.chatHandle
+		var id = "hairColorID" + player.id
 		var html = "<input id = '" + id + "' type='color' name='favcolor' value='" + player.hairColor + "'>"
 		return html;
 	}
 	this.drawOneHairColorDropDownOLD = function(player){
-		var html = "<select id = 'hairColorID" + player.chatHandle + "' name='hairColor" +player.chatHandle +"'>";
+		var html = "<select id = 'hairColorID" + player.id + "' name='hairColor" +player.id +"'>";
 		for(var i = 0; i< human_hair_colors.length; i++){
 			if(human_hair_colors[i] == player.hairColor){
 				html += '<option style="background:' + human_hair_colors[i] + '" selected = "hairColor" value="' + human_hair_colors[i] +'">' + human_hair_colors[i]+'</option>'
@@ -324,7 +324,7 @@ function CharacterCreatorHelper(players){
 	}
 
 	this.drawOneBloodColorDropDown = function(player){
-		var html = "<select id = 'bloodColorID" + player.chatHandle + "' name='bloodColor" +player.chatHandle +"'>"
+		var html = "<select id = 'bloodColorID" + player.id + "' name='bloodColor" +player.id +"'>"
 		for(var i = 0; i< bloodColors.length; i++){
 			if(bloodColors[i] == player.bloodColor){
 				html += '<option style="background:' + bloodColors[i] + '" selected = "bloodColor" value="' + bloodColors[i] +'">' + bloodColors[i]+'</option>'
@@ -343,7 +343,7 @@ function CharacterCreatorHelper(players){
 	this.drawOneAspectDropDown = function(player){
 		available_aspects = nonrequired_aspects.slice(0); //required_aspects
 	  available_aspects = available_aspects.concat(required_aspects.slice(0));
-		var html = "<select id = 'aspectID" + player.chatHandle + "'' name='aspect" +player.chatHandle +"'>";
+		var html = "<select id = 'aspectID" + player.id + "'' name='aspect" +player.id +"'>";
 		for(var i = 0; i< available_aspects.length; i++){
 			if(available_aspects[i] == player.aspect){
 				html += '<option selected = "selected" value="' + available_aspects[i] + '" >' + available_aspects[i]+'</option>'
