641f7327cebc1d0b97eb015b438f4adec9edb52a
Jenny <gtg287y@gmail.com>
2017-06-07 18:20:58 -0400

    ;alskdjf

--- javascripts/Scenes/CorpseSmooch.js
+++ javascripts/Scenes/CorpseSmooch.js
@@ -174,7 +174,7 @@ function CorpseSmooch(session){
 					if(alt && alt.alternateScene(div)){
 						//do nothing here.
 					}else{
-						ret += "<Br><Br><img src = 'images/sceneIcons/death_icon.png'>" + d.htmlTitle() + "'s corpse waits patiently for the kiss of life. But nobody came. ";
+						ret += "<Br><Br><img src = 'images/sceneIcons/death_icon.png'>" + d.htmlTitle() + "'s waits patiently for the kiss of life. But nobody came. ";
 						ret += " Their dream self dies as well. ";
 						this.makeDead(d);
 					}
@@ -222,7 +222,7 @@ function CorpseSmooch(session){
 				this.makeAlive(d);
 				combo ++;
 			}else{
-				ret += d.htmlTitle() + "'s corpse waits patiently for the kiss of life. But nobody came. ";
+				ret += d.htmlTitle() + "'s waits patiently for the kiss of life. But nobody came. ";
 				ret += " Their dream self dies as well. ";
 				this.makeDead(d);
 			}
