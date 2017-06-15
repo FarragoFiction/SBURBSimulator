6acaf0122879c835d1ee750b6dbead4d3e28f30d
JR <jennifer.schultz@ericsson.com>
2017-06-15 15:52:32 -0400

    just gotta debug AB and ready for experimental release

--- javascripts/v2.0/char_viewer.js
+++ javascripts/v2.0/char_viewer.js
@@ -7,7 +7,7 @@ window.onload = function() {
 	loadNavbar();
 	var params = window.location.href.substr(window.location.href.indexOf("?")+1)
 	if (params == window.location.href) params = ""
-	$("#character_creator").append("<a target='_blank' href = 'index2.html?selfInsertOC=true&" + params + "'>Send Random Fan OCs From This Category Into SBURB?</a><Br>");
+	$("#character_creator").append("<a target='_blank' href = 'index2.html?selfInsertOC=true&" + params + "'>Send Random Fan OCs From This Category Into SBURB?</a> &nbsp &nbsp &nbsp<a target='_blank' href = 'rare_session_finder.html?selfInsertOC=true&" + params + "'>AB Report For Fan OCs From This Category</a><Br>");
 	loadFuckingEverything(true);
 
 
