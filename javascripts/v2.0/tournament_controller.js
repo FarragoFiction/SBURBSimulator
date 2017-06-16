843c965ca002b2360618632ba622e66ddecdd283
JR <jennifer.schultz@ericsson.com>
2017-06-16 09:39:35 -0400

    team selector

--- javascripts/v2.0/tournament_controller.js
+++ javascripts/v2.0/tournament_controller.js
@@ -5,12 +5,23 @@ var simulationMode  = false;
 window.onload = function() {
 	$(this).scrollTop(0);
 	loadNavbar();
-	renderHeader();
-	var params = window.location.href.substr(window.location.href.indexOf("?")+1)
-	if (params == window.location.href) params = ""
-	$("#character_creator").append("<a target='_blank' href = 'index2.html?selfInsertOC=true&" + params + "'>Send Random Fan OCs From This Category Into SBURB?</a> &nbsp &nbsp &nbsp<a target='_blank' href = 'rare_session_finder.html?selfInsertOC=true&" + params + "'>AB Report For Fan OCs From This Category</a><Br><Br><Br>");
-	loadFuckingEverything(true);
+	displayPotentialFighters();
+}
 
-
-
+//all selected tiers will render example chars and a text explanation.handle up to ALL chosen.
+//KnightStuck: randomly generated players that are forced to be Knights only.
+//IF use rare session finder, will have to have params ALSO able to come internally, not just from url 
+//fighters chosen become a "bracket"
+//do i want symbols for teams?
+function displayPotentialFighters(){
+	//tiers are EXACTLY the same name as the param, so can generate a "URL" of the right format, for AB to check.
+	var fanTeams = ["reddit","tumblr","discord","creditsBuckaroos","ideasWranglers","patrons","patrons2","patrons3","canon","otherFandoms","creators"];
+	var classTeams = ["KnightStuck", "SeerStuck","BardStuck","HeirStuck","MaidStuck","RogueStuck","PageStuck","ThiefStuck","SylphStuck","PrinceStuck","WitchStuck","MageStuck"];
+	var teams = fanTeams.concat(classTeams);
+	var html = "<select multiple size = '" + teams.length + "' id = 'tiers' name='tier'>";
+	for(var i = 0; i< teams.length; i++){
+			html += '<option value="' + teams[i] +'">' + teams[i]+'</option>'
+	}
+	html += '</select>'
+	$("#teams").append(html);
 }
