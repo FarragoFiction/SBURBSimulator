8671e6d610f49e763d2c7de156159e6153dd175b
Jenny <gtg287y@gmail.com>
2017-06-18 20:06:58 -0400

    simple image browser

--- javascripts/v2.0/image_browser.js
+++ javascripts/v2.0/image_browser.js
@@ -13,15 +13,15 @@ function renderAllHair(){
 	var maxHair = 60;
 	//don't use canvas, but still layer?  bg, like for KR.
 	for(var i = minHair; i<= maxHair; i++){
-			renderLayeredSprites(["images/Hair/hair_back"+i+".png","images/Hair/hair"+i+".png"]);
+			renderLayeredSprites([new SpritePart("images/Hair/hair_back"+i+".png", "Hair " +i),new SpritePart("images/Hair/hair"+i+".png", "")]);
 	}
 }
 
 function renderAllHorns(){
 	var minHorn = 1
-	var maxHorn = 60;
+	var maxHorn = 46;
 	for(var i = minHorn; i<= maxHorn; i++){
-			renderLayeredSprites(["images/Horns/left"+i+".png","images/Horns/right"+i+".png"]);
+			renderLayeredSprites([new SpritePart("images/Horns/left"+i+".png","leftHorn "+i),new SpritePart("images/Horns/right"+i+".png", "rightHorn" +i)]);
 	}
 }
 
@@ -29,8 +29,14 @@ function renderAllHorns(){
 function renderLayeredSprites(spriteArray){
 	var html = "<div class = 'spriteParent'>"; //all images should be rendered at same position in sprite parent
 	for(var i = 0; i<spriteArray.length; i++){
-		html += "<img class = 'spriteImg' src = '" + spriteArray[i] + "'></img>";
+		html += "<img class = 'spriteImg' src = '" + spriteArray[i].location + "'></img><br>"+spriteArray[i].name;
 	}
 	html += "</div>"
 	$("#images").append(html);
+}
+
+
+function SpritePart(location, name){
+	this.location = location;
+	this.name = name;
 }
