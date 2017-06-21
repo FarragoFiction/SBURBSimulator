0953873f5d298aee4596ae0cb80b250ba994b9e3
Jenny <gtg287y@gmail.com>
2017-06-21 07:52:46 -0400

    neurotically commiting consantly because am intimdated by how ambitious i'm being

--- javascripts/handle_spritesRefactoring.js
+++ javascripts/handle_spritesRefactoring.js
@@ -2,13 +2,25 @@
 function RenderingEngine(dontRender, defaultRendererID){
   this.dontRender = dontRender; //AB for example doesn't want you to render
   this.defaultRendererID = defaultRendererID;
-  this.rendererHelper = new RendererHelper();
-  this.renderers = [null, new HomestuckRenderer(this.rendererHelper), new EggRenderer(this.rendererHelper)]; //if they try to render with "null", use defaultRendererID index instead.
+  this.renderers = [null, new HomestuckRenderer(this), new EggRenderer(this)]; //if they try to render with "null", use defaultRendererID index instead.
+
+  //need to be kept as high up as possible so that rest of sim can access these convinience methods
+  this.hexToRgbA = function(hex){
+    var c;
+    if(/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)){
+        c= hex.substring(1).split('');
+        if(c.length== 3){
+            c= [c[0], c[0], c[1], c[1], c[2], c[2]];
+        }
+        c= '0x'+c.join('');
+        //return 'rgba('+[(c>>16)&255, (c>>8)&255, c&255].join(',')+',1)';
+        return [(c>>16)&255, (c>>8)&255, c&255]
+    }
+    throw new Error('Bad Hex ' + hex);
+  }
 }
 
-function RendererHelper(){
 
-}
 
 
 //calls either HomestuckHumanRenderer or HomestuckTrollRenderer depending on isTroll.
@@ -23,5 +35,10 @@ function HomestuckHumanRenderer(rh){
 }
 
 function HomestuckTrollRenderer(rh){
+  this.rendererHelper = rh;
+}
+
+
+function EggRenderer(rh){
   this.rendererHelper = rh;
 }
