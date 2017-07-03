5c738fc998c96c89438d14ce0fa4e0e4bdd21c0d
Jenny <gtg287y@gmail.com>
2017-07-03 10:54:36 -0400

    flavor text

--- javascripts/Scenes/DoLandQuest.js
+++ javascripts/Scenes/DoLandQuest.js
@@ -68,28 +68,30 @@ function DoLandQuest(session){
 			f = this.session.fraymotifCreator.makeFraymotifForPlayerWithFriends(player, helper, 1);
 		}
 		player.fraymotifs.push(f);
-		return this.fraymotifFlavorTextForPlayer(player, f);
+
+
+		return this.fraymotifFlavorTextForPlayer(player, f)
 
 	}
 
 	this.fraymotifFlavorTextForPlayer = function(player, fraymotif){
-		var normalWays = ["The " + player.htmltTitle() + " purchases " + fraymotif.name + "from the fraymotif store like a sensible person. "];
-		normalWays.push("The " + player.htmltTitle() + " has finally saved up enough boondollars to buy " + fraymotif.name + "from the fraymotif store. ");
-		if(randomNumber > 0.5) return getRandomElementFromArray(normalWays);
+		var normalWays = ["The " + player.htmlTitle() + " purchases " + fraymotif.name + "from the fraymotif store like a sensible person. "];
+		normalWays.push("The " + player.htmlTitle() + " has finally saved up enough boondollars to buy " + fraymotif.name + "from the fraymotif store. ");
+		if(Math.seededRandom() > 0.5) return getRandomElementFromArray(normalWays);
 		//otherwise do special shit.
-		if(player.class_name == "Thief") return "The " + player.htmltTitle() + " blatantly robs the fraymotif store, scoring " + fraymotif.name + ".  I sure hope it's worth the risk of being put in the slammer. "
-		if(player.class_name == "Rogue") return "The " + player.htmltTitle() + " daringly heists fraymotif store, scoring " + fraymotif.name + ".  Nobody will notice it's missing for days. "
-		if(player.class_name == "Knight") return "The " + player.htmltTitle() + " vallantly defends the fraymotif store from underlings, scoring " + fraymotif.name + " as a reward. "
-		if(player.class_name == "Seer") return "The " + player.htmltTitle() + " is made aware that " + fraymotif.name + " is hidden in a secret location, and claims it. "
-		if(player.class_name == "Bard") return "The " + player.htmltTitle() + " just suddenly knows " + fraymotif.name + ".  I sure hope you aren't expecting to find out HOW they learned it. "
-		if(player.class_name == "Page") return "The " + player.htmltTitle() + " has worked hard and put in the hours and now knows " + fraymotif.name + ".  Sometimes hard work pays off!. "
-		if(player.class_name == "Sylph") return "The " + player.htmltTitle() + " has built up a report with the consort running the fraymotif shop. They receive " + fraymotif.name + " on the house. "
-		if(player.class_name == "Heir") return "The " + player.htmltTitle() + " finds out that " + fraymotif.name + " was bequeathed to them by a wealthy consort. Their death will not be in vain!  "
-		if(player.class_name == "Maid") return "The " + player.htmltTitle() + " helps the consort that runs the fraymotif shop organize everything.  They are rewarded with " + fraymotif.name + ". "
-		if(player.class_name == "Prince") return "The " + player.htmltTitle() + " scores " + fraymotif.name + " for a stupidly discounted 'going out of business' price. "
-		if(player.class_name == "Witch") return "The " + player.htmltTitle() + " joins a coven of Secret Wizards to learn " + fraymotif.name + ".   "
-		if(player.class_name == "Mage") return "The " + player.htmltTitle() + " is pretty sure they have figured out the fraymotif system, at least enough to learn " + fraymotif.name + ". "
-
+		if(player.class_name == "Thief") return "The " + player.htmlTitle() + " blatantly robs the fraymotif store, scoring " + fraymotif.name + ".  I sure hope it's worth the risk of being put in the slammer. "
+		if(player.class_name == "Rogue") return "The " + player.htmlTitle() + " daringly heists fraymotif store, scoring " + fraymotif.name + ".  Nobody will notice it's missing for days. "
+		if(player.class_name == "Knight") return "The " + player.htmlTitle() + " vallantly defends the fraymotif store from underlings, scoring " + fraymotif.name + " as a reward. "
+		if(player.class_name == "Seer") return "The " + player.htmlTitle() + " is made aware that " + fraymotif.name + " is hidden in a secret location, and claims it. "
+		if(player.class_name == "Bard") return "The " + player.htmlTitle() + " just suddenly knows " + fraymotif.name + ".  I sure hope you aren't expecting to find out HOW they learned it. "
+		if(player.class_name == "Page") return "The " + player.htmlTitle() + " has worked hard and put in the hours and now knows " + fraymotif.name + ".  Sometimes hard work pays off!. "
+		if(player.class_name == "Sylph") return "The " + player.htmlTitle() + " has built up a report with the consort running the fraymotif shop. They receive " + fraymotif.name + " on the house. "
+		if(player.class_name == "Heir") return "The " + player.htmlTitle() + " finds out that " + fraymotif.name + " was bequeathed to them by a wealthy consort. Their death will not be in vain!  "
+		if(player.class_name == "Maid") return "The " + player.htmlTitle() + " helps the consort that runs the fraymotif shop organize everything.  They are rewarded with " + fraymotif.name + ". "
+		if(player.class_name == "Prince") return "The " + player.htmlTitle() + " scores " + fraymotif.name + " for a stupidly discounted 'going out of business' price. "
+		if(player.class_name == "Witch") return "The " + player.htmlTitle() + " joins a coven of Secret Wizards to learn " + fraymotif.name + ".   "
+		if(player.class_name == "Mage") return "The " + player.htmlTitle() + " is pretty sure they have figured out the fraymotif system, at least enough to learn " + fraymotif.name + ". "
+		return "CLASS UNDEFINED DUNKASS"
 	}
 
 
