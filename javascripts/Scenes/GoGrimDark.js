6110a3118e61b2520896af3a86ed4a0fed36ffe1
JR <jennifer.schultz@ericsson.com>
2017-07-05 16:01:06 -0400

    stubborn throes

--- javascripts/Scenes/GoGrimDark.js
+++ javascripts/Scenes/GoGrimDark.js
@@ -58,6 +58,7 @@ function GoGrimDark(session){
 				ret += " The " + this.player.htmlTitleBasic() + " will tell anyone who will listen that the game needs to be broken. "
 			}else if(this.player.grimDark == 4){
 				console.log("full grim dark: " + this.session.session_id)
+				//alert("full grim dark: " + this.session.session_id)
 				this.player.increasePower(200);
 				ret +=  "The " + this.player.htmlTitleBasic() + " slips into the fabled blackdeath trance of the woegothics, quaking all the while in the bloodeldritch throes of the broodfester tongues.";
 				ret += " It is now painfully obvious to anyone with a brain, they have basically gone completely off the deep end in every way. The "
