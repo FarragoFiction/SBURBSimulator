6dfa09dda023d45abd091f134d7e7da26b0c4807
Jenny <gtg287y@gmail.com>
2017-07-11 19:40:05 -0400

    better seerOfVoid description

--- javascripts/Scenes/Intro.js
+++ javascripts/Scenes/Intro.js
@@ -715,12 +715,12 @@ function Intro(session){
 				narration += " They appear to be destined for greatness. ";
 			}
 
-			if(this.player.minLuck + this.player.maxLuck >15){
+			if(this.player.minLuck + this.player.maxLuck >25){
 				//console.log("initially lucky player: " +this.session.session_id)
 				narration += " They have aaaaaaaall the luck. All of it.";
 			}
 
-			if(this.player.maxLuck < -15){
+			if(this.player.maxLuck < -25){
 				//console.log("initially unlucky player: " +this.session.session_id)
 				narration += " They have an insurmountable stockpile of TERRIBLE LUCK.";
 			}
