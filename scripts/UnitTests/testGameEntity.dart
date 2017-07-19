6c8df0c0a13c99ded334b85b2cdfbaf3eeb2b7dd
JR <jennifer.schultz@ericsson.com>
2017-07-19 11:52:27 -0400

    managed to find my first bug, set stats hash seems to not be working

--- scripts/UnitTests/testGameEntity.dart
+++ scripts/UnitTests/testGameEntity.dart
@@ -9,6 +9,7 @@ main() {
   print("Hello World");
   testName();
   testID();
+  testStats();
 }
 
 setup() {
@@ -25,4 +26,27 @@ testID() {
   setup();
   print(testGE.id);
   assert(testGE.id == 413 ? true : throw "ID should be 413, but is: ${testGE.id}");
+}
+
+testStats() {
+  setup();
+  print(testGE.stats);
+  assert(testGE.getStat("hp") == 0 ? true : throw "initial hp should be 0, but is: ${testGE..getStats("hp")}");
+  testGE.setStatsHash({"hp": 100, "currentHP":10, "power":3, "maxLuck": 100});
+  jRAssert("hp", testGE.getStat("hp"), 100);
+  jRAssert("currentHP", testGE.getStat("currentHP"), 10);
+  jRAssert("power", testGE.getStat("power"), 3);
+  jRAssert("maxLuck", testGE.getStat("maxLuck"), 100);
+  jRAssert("minLuck", testGE.getStat("minLuck"), 0);  //confirm did not change.
+  jRAssert("sanity", testGE.getStat("sanity"), 0);  //confirm did not change.
+  jRAssert("alchemy", testGE.getStat("alchemy"), 0);  //confirm did not change.
+  jRAssert("RELATIONSHIPS", testGE.getStat("RELATIONSHIPS"), 0);  //confirm did not change.
+  jRAssert("freeWill", testGE.getStat("freeWill"), 0);  //confirm did not change.
+  jRAssert("mobility", testGE.getStat("mobility"), 0);  //confirm did not change.
+  print(testGE.stats);
+}
+
+jRAssert(name, tested, expected)
+{
+  assert(tested == expected ? true : throw "${name} should be ${expected}, but is: ${tested}");
 }
