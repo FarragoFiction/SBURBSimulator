14d137e1b70772226f8696bf4a91efbf2961484c
Unknown <emperorlexx@hotmail.com>
2017-07-31 00:32:43 +0100

    More colour stuff

--- web/tools/aspect_palette/aspect_palette.dart
+++ web/tools/aspect_palette/aspect_palette.dart
@@ -8,11 +8,27 @@ import "../../scripts/includes/colour_picker.dart";
 
 
 void main() {
-    CanvasElement canvas = querySelector("#canvas");
+    Element stuff = querySelector("#stuff");
+
+    for (int i=0; i<100; i++) {
+        stuff.append(makeGradientSwatch());
+    }
+}
+
+CanvasElement makeGradientSwatch() {
+    CanvasElement canvas = new CanvasElement(width: 200, height:200);
     CanvasRenderingContext2D ctx = canvas.context2D;
 
-    Colour col1 = new Colour.fromHex(0xFF0000);
-    Colour col2 = new Colour.fromHex(0x00FF00);
+    Random rand = new Random();
+
+    double brightness = rand.nextDouble() * 0.4 + 0.5;
+    double sat = rand.nextDouble() * 0.2 + 0.5;
+
+    Colour col1 = new Colour.hsv(rand.nextDouble(), rand.nextDouble() * 0.1 + sat, rand.nextDouble() * 0.3 + brightness);
+    Colour col2 = new Colour.hsv(rand.nextDouble(), rand.nextDouble() * 0.1 + sat, rand.nextDouble() * 0.3 + brightness);
+
+    //Colour col1 = new Colour.fromHex(0xFF0000);
+    //Colour col2 = new Colour.fromHex(0x00FF00);
 
     int w = canvas.width;
     int h = canvas.height;
@@ -25,7 +41,7 @@ void main() {
 
             double frac = x / (w*2) + y / (h*2);
 
-            Colour mix = col1.mix(col2, frac);//, true);
+            Colour mix = col1.mix(col2, frac, true);
 
             data.data[index] = mix.red;
             data.data[index+1] = mix.green;
@@ -35,4 +51,6 @@ void main() {
     }
 
     ctx.putImageData(data, 0, 0);
+
+    return canvas;
 }
