5714b32c0fc24243106bbaec84827428aeef8bbe
Jenny <gtg287y@gmail.com>
2017-07-30 10:16:11 -0400

    abj isn't obssessed with fanon anymore

--- web/scripts/Controllers/SimController.dart
+++ web/scripts/Controllers/SimController.dart
@@ -176,7 +176,6 @@ abstract class SimController {
   }
 
   void reinit() {
-    print("classes are $classes");
     available_classes = new List<String>.from(classes);
     available_aspects = new List<String>.from(nonrequired_aspects);
     available_aspects.addAll(required_aspects);
