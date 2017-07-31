cb9ed0a482616123d5bede3635aad0640b10e476
Jenny <gtg287y@gmail.com>
2017-07-30 21:00:38 -0400

    workign on newsposts

---
+++ web/scripts/Controllers/NewspostController.dart
@@ -0,0 +1,7 @@
+//this is in charge of the main page newsposts, and the Author/ABJ newsposts.
+//for now, just get main page working. (so no simulator madness)
+import 'dart:html';
+import 'StoredNewsposts.dart';
+main() {
+
+}
