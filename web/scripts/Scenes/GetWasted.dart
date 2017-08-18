a71a3bd4c9d37608fdc3ff3193f335dfb0ffcfc0
JadedResearcher <gtg287y@gmail.com>
2017-08-18 10:10:31 -0400

    omg, what is this???

---
+++ web/scripts/Scenes/GetWasted.dart
@@ -0,0 +1,39 @@
+import "dart:html";
+import "../SBURBSim.dart";
+/*
+    These are how Wastes, and things that aspire to be Wastes, do their shit.
+    sburbLore and Gnosis will function similarly to corruption and GrimDark.
+
+    sburbLore will have a max value, and if it goes over that, gnosis goes up by 1
+    This scene is the parallel of goGrim dark. When you go up by a gnosis level, this scene
+    will say shit about it.  It will use Session Mutator to
+
+ */
+
+class GetWasted extends Scene {
+    Player player; //only one player can get wasted at a time.
+    int tippingPointBase = 1000;
+    GetWasted(Session session): super(session);
+
+  @override
+  bool trigger(List<Player> playerList){
+      this.playerList = playerList;
+      this.player = null;
+      for(Player p in session.availablePlayers){ //unlike grim dark, corpses are not allowed to have eureka moments.
+          if(this.loreReachedTippingPoint(p)){
+              this.player = p;
+              return true;
+          }
+      }
+      return false;
+  }
+
+  bool loreReachedTippingPoint(Player p){
+      return p.sburbLore >= tippingPointBase * p.gnosis //linear growth, but the base is high.
+  }
+
+  @override
+  void renderContent(Element div) {
+      appendHtml(div,"OMFG, THIS WOULD DO SOMETHING IF JR WASN'T A LAZY PIECE OF SHIT. ";
+  }
+}
