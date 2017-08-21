51b1bcea4a39b79446ab30a27f96f1b4557dc6ad
JadedResearcher <gtg287y@gmail.com>
2017-08-21 14:13:51 -0400

    outter tag

--- test/testFAQFile.dart
+++ test/testFAQFile.dart
@@ -6,7 +6,7 @@ import "../web/scripts/FAQEngine/FAQFile.dart";
 //god, i hate that i can't do simple unit tests if any html gets anywhere, so can't test file loading yet.
 //experimenting with different scripting bs. should only need two tags for now.
 //TODO might not work for real one. \n in here breaks it, will actual new lines break it from html? either way probably should replace with <Br>
-String pretendFileContents = "<section><header>Question: Does this parser work?  </header><body>Apparently.</body></section><section><header>Question: How bullshit is everything.</header><body>Extremely.</body></section>";
+String pretendFileContents = "<faq><section><header>Question: Does this parser work?  </header><body>Apparently.</body></section><section><header>Question: How bullshit is everything.</header><body>Extremely.</body></section></faq>";
 
 main() {
     print("Hello World");
