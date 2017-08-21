5a80df939d75376e4f853377f9ffa5cb15e388c9
JadedResearcher <gtg287y@gmail.com>
2017-08-21 14:01:10 -0400

    trying to figure out why xml is yelling at me again

--- web/scripts/FAQEngine/FAQSection.dart
+++ web/scripts/FAQEngine/FAQSection.dart
@@ -1,3 +1,4 @@
+import 'package:xml/xml.dart' as Xml;
 ///a section of a faq, can belong to a FAQFile or a GeneratedFAQ
 class FAQSection {
     ///what is the question or section name? How do I x vs How to X
@@ -18,6 +19,8 @@ class FAQSection {
 
     ///assume sections start with <section> and have no ending tag cuz i am lazy
     static List<String> mainTextToSubStrings(String text) {
+        Xml.XmlDocument document = Xml.parse(text);
+        print("children are ${document.children}");
         return text.split("<section>");
     }
 }
