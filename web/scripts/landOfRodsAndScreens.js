33bae487047e786025c7f6474598d58eddb967da
Jenny <gtg287y@gmail.com>
2017-08-24 18:45:24 -0400

    LORAS and half the rage faq

--- web/scripts/landOfRodsAndScreens.js
+++ web/scripts/landOfRodsAndScreens.js
@@ -1,5 +1,5 @@
 var screens = [];
-var maxState = 140; //this number MUST be even.
+var maxState = 142; //this number MUST be even.
 var imagesWaiting = 0;
 var imagesLoaded = 0;
 var targetImage = 0; //what could control this???
