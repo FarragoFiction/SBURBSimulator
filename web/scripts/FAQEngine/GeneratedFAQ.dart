4fe50cc53bb484a537cd26cb55cafc852847e956
JadedResearcher <gtg287y@gmail.com>
2017-08-24 15:00:01 -0400

    gonna shift focus to misc faqs

--- web/scripts/FAQEngine/GeneratedFAQ.dart
+++ web/scripts/FAQEngine/GeneratedFAQ.dart
@@ -32,7 +32,7 @@ class GeneratedFAQ {
         print("I'm making html for a generated faq with ${sections.length} sections");
         Quirk q = author.quirk;
         asciiHeader = GeneratedFAQ.pickASCIIHeaderFromSections(rand, sections);
-        String ret =  "<button class='red_x'id = 'close$id'>X</button><br><br>$asciiHeader<Br><Br><center>By ${author.chatHandle}</center>";
+        String ret =  "<button class='red_x'id = 'close$id'>X</button><br><br><div class = 'ascii'>$asciiHeader</div><Br><Br><center>By ${author.chatHandle}</center>";
         for(FAQSection s in sections) {
             ret = "$ret <br><Br>${symbol*amount}${q.translate(s.header)}${symbol*amount}<br><br>${q.translate(s.body)}<br><Br>";
         }
