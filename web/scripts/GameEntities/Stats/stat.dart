0ad49e01f65b2f753f21102126d716fc92a08025
Unknown <emperorlexx@hotmail.com>
2017-08-29 06:29:22 +0100

    stat notes

--- web/scripts/GameEntities/Stats/stat.dart
+++ web/scripts/GameEntities/Stats/stat.dart
@@ -10,3 +10,18 @@ class Stat {
 
     double derived(StatHolder stats) { return stats[this]; }
 }
+
+/*
+NOTES
+
+Stat processing order: classpect.modifiers(stat.modifier(baseline + growth + buff flat values)) * buff multipliers
+maybe?
+
+interests provide one time buff and +mult?
+god tier becomes smaller flat buff with multiplier instead of gains
+experience stat gives multiplier to stats at stat modifier stage
+
+be sparing with flat buffs, they could get very powerful
+
+prototyping as buffs, prototype buff referencing another StatHolder or StatOwner maybe
+ */
