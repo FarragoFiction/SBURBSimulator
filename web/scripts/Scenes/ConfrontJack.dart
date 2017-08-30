c07b11bf5771cc4018aea2a108166e01950ddb82
aCactusOrSomething <31144053+aCactusOrSomething@users.noreply.github.com>
2017-08-30 01:09:53 -0500

    ConfrontJack now works, but jack remains after being exiled.

--- web/scripts/Scenes/ConfrontJack.dart
+++ web/scripts/Scenes/ConfrontJack.dart
@@ -1,38 +1,54 @@
 import "dart:html";
 import "../SBURBSim.dart";
 
-class ConfrontJack extends Scene{
-  List<Player> playerList = [];  //what players are already in the medium when i trigger?
+class ConfrontJack extends Scene {
+  List<Player> playerList = [
+  ]; //what players are already in the medium when i trigger?
   bool canRepeat = false;
   Player player = null;
 
-  ConfrontJack(Session session): super(session, false);
+  ConfrontJack(Session session) : super(session, false);
 
   @override
-  bool trigger(List<Player> playerList){
+  bool trigger(List<Player> playerList) {
     this.playerList = playerList;
 
-    this.player = findAspectPlayer(this.session.availablePlayers, Aspects.MIGHT);
+    this.player =
+        findAspectPlayer(this.session.availablePlayers, Aspects.MIGHT);
     if (this.player == null) {
       return false;
-    }else {
-      return (this.player != null && !this.session.jack.exiled) && (this.session.jack.getStat("currentHP") >  0 );
+    } else {
+      return (this.player != null && !this.session.jack.exiled) && (this.session.jack.getStat("currentHP") > 0);
     }
   }
 
   @override
-  void renderContent(Element div){
-    appendHtml(div,"<br> <img src = 'images/sceneIcons/jack_icon.png'> ");
-  }
-  dynamic content(Element div){
-    String ret = "Hey, " + this.player.htmlTitle() + " is trying to fight Jack. It's a shame CACTUS IS INCOMPETENT, or else this would, i dunno, work.";
-    Team pTeam = new Team.withName(this.player.htmlTitle(),this.session, this.session.availablePlayers);
+  void renderContent(Element div) {
+    appendHtml(div, "<br> <img src = 'images/sceneIcons/jack_icon.png'> ");
+    //appendHtml(div);
+    String ret = "Hey, " + this.player.htmlTitle() +
+        " is trying to fight Jack. It's a shame CACTUS IS INCOMPETENT, or else this would, i dunno, work.";
+    appendHtml(div, ret);
+    Team pTeam = new Team.withName(
+        this.player.htmlTitle(), this.session, [this.player]);
     Team dTeam = new Team(this.session, [this.session.jack]);
     Strife strife = new Strife(this.session, [pTeam, dTeam]);
-
     strife.startTurn(div);
-    appendHtml(div,""+ret);
-    return div;
-  }
+    if (this.session.jack.getStat("currentHP") <= 0){
+      this.session.jack.exiled = true;
+      String winText = "The strife with Jack was successful. His corpse has been exiled, never to be seen again.";
+      appendHtml(div, winText);
+    };
 
+    return;
   }
+}
+  /*dynamic content(Element div){
+
+
+
+
+    return div;
+  }*/
+
+  //}
