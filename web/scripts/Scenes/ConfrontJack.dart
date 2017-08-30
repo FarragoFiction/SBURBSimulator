d55afb2db34cb8798003079b0a1dd8dd60289596
aCactusOrSomething <31144053+aCactusOrSomething@users.noreply.github.com>
2017-08-30 00:22:47 -0500

    trying to get this to work

--- web/scripts/Scenes/ConfrontJack.dart
+++ web/scripts/Scenes/ConfrontJack.dart
@@ -16,17 +16,23 @@ class ConfrontJack extends Scene{
     if (this.player == null) {
       return false;
     }else {
-      return (this.player != null);
-    }// && !this.session.jack.exiled) && (this.session.jack.getStat("currentHP") >  0 ));
+      return (this.player != null && !this.session.jack.exiled) && (this.session.jack.getStat("currentHP") >  0 );
+    }
   }
 
   @override
   void renderContent(Element div){
-    appendHtml(div,"<br> <img src = 'images/sceneIcons/jack_icon.png'> "+this.content());
+    appendHtml(div,"<br> <img src = 'images/sceneIcons/jack_icon.png'> ");
   }
-  dynamic content(){
-    String ret = "Hey, " + this.player.htmlTitle() + " is trying to fight Jack. It's a shame CACTUS IS INCOMPETENT.";
-    return ret;
+  dynamic content(Element div){
+    String ret = "Hey, " + this.player.htmlTitle() + " is trying to fight Jack. It's a shame CACTUS IS INCOMPETENT, or else this would, i dunno, work.";
+    Team pTeam = new Team.withName(this.player.htmlTitle(),this.session, this.session.availablePlayers);
+    Team dTeam = new Team(this.session, [this.session.jack]);
+    Strife strife = new Strife(this.session, [pTeam, dTeam]);
+
+    strife.startTurn(div);
+    appendHtml(div,""+ret);
+    return div;
   }
 
   }
