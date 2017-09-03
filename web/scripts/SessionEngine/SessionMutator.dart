da735f70e40f67044059ea89ff67553f7401c578
Jenny <gtg287y@gmail.com>
2017-09-03 10:43:47 -0400

    breath4 is ready to test

--- web/scripts/SessionEngine/SessionMutator.dart
+++ web/scripts/SessionEngine/SessionMutator.dart
@@ -225,15 +225,19 @@ class SessionMutator {
   }
 
   String breath(Session s, Player activatingPlayer) {
-    return abjectFailure(s, activatingPlayer);
     effectsInPlay ++;
-      /*
-        TOOD:
-        * available players is always all players.
-        * all quest chains are active (npc shit)
-        * *uses true random instead of seed, for freedom from story
-       */
-
+    breathField = true;
+    s.rand = new Random(); //breath gets freedom from narrative, true random now, no predictabilitiy.
+    //show off that new true randomness:
+    String ret = "The $activatingPlayer begins to glow. Lines of code appear dramatically behind them. ";
+    ret += s.rand.pickFrom(<String> ["A wave of mobility washes over SBURB.","All players feel strangely mobile."," SBURB suddenly cares a LOT more about getting the plot moving forwards."]);
+    ret += s.rand.pickFrom(<String>[" Somewhere in the distance, you can hear the AuthorBot cursing."," SBURBs narrative control has slipped the reigns entirely. Sessions now have the freedom to do whatever they want."," Huh. Wait. Is this really canon? The Observer visible timeline isn't supposed to go this way, right? "]);
+    //TODO once npcs quests are a thing, need to have all active at once.
+    ret += " You suddenly get the strange feeling that this session has become a LOT less shareable.";
+    for(Player p in s.players) {
+      p.addStat("mobility", 413);  //not a hope level of boost, but enough to probably fight most things
+    }
+    return ret;
   }
 
   String light(Session s, Player activatingPlayer) {
