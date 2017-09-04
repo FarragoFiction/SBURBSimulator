f8271e2b63b67f49912849d15778e67d20acacda
aCactusOrSomething <31144053+aCactusOrSomething@users.noreply.github.com>
2017-09-04 17:45:44 -0500

    Rain clones now work.

--- web/scripts/Scenes/RainClone.dart
+++ web/scripts/Scenes/RainClone.dart
@@ -1,8 +1,8 @@
 import "dart:html";
 import "../SBURBSim.dart";
 
-class RainClone extends Scene {
 
+class RainClone extends Scene {
   List<Player> playerList = []; //what players are already in the medium when i trigger?
   bool canRepeat = false; //todo: change such that this can repeat 2-3 times in
   Player player = null;
@@ -21,22 +21,50 @@ class RainClone extends Scene {
       return (this.player != null);
     }
   }
-
   @override
   void renderContent(Element div) {
     appendHtml(div,"<br>");
     var divID = (div.id);
     String canvasHTML = "<br><canvas id='canvas" + divID+"' width='" +canvasWidth.toString() + "' height="+canvasHeight.toString() + "'>  </canvas>";
     appendHtml(div,canvasHTML);
+
     var canvas = querySelector("#canvas"+ divID);
     Drawing.drawGodSymbolBG(canvas, this.player);
     Drawing.drawSprite(canvas, this.player);
     Drawing.drawSpriteTurnways(canvas, this.player);
-    String ret = "The " + this.player.htmlTitle() +
-        " Just made a clone of themselves? WTF?";
+
+    String ret = "The " + this.player.htmlTitle() + " Just made a clone of themselves?";
     appendHtml(div, ret);
+
     //todo: make it hapen.
-    ret = "<br> ...Oh. It seems that AC has not programmed this yet. The new copy of the " + this.player.htmlTitleBasic() + " looks dissapointed, and wanders off to some obscure corner of the medium.";
+    Player rainyClone = new Player();
+    rainyClone = this.player.clone();
+    rainyClone.session = this.player.session;
+    rainyClone.denizen = this.player.denizen;//clones don't get their own denizen. hopefully, this wont exile the Original's Denizen...
+    rainyClone.relationships = this.player.relationships;//todo:
+
+
+    Relationship.transferFeelingsToClones(this.player, [rainyClone]);
+    for (int j = 0; j < this.session.players.length; j++) {
+      this.player = this.session.players[j];
+      player.generateRelationships([rainyClone]);
+    } //should have neutral relationship with self.
+
+    rainyClone.guardian = this.player;
+    for (int i = 0; i < playerList.length; i++) {
+      GameEntity g = playerList[i]; //could be a sprite, and they don't have classpects.
+      if (g is Player) {
+        Player p = playerList[i];
+        if (p == this.player) {
+          super.session.players.insert(i, rainyClone);
+        }
+      }
+    }
+
+    ret = "<br> ...Oh. It seems that this has not been programmed yet. The new copy of the " + rainyClone.htmlTitleBasic() + " looks dissapointed, and wanders off to some obscure corner of the medium.";
     appendHtml(div, ret);
+    session.logger.info("tried to clone Rain player.");
+
+
   }
 }
