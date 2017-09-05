61126887af4226d70c764c7f06250ddf3cab8e7a
Jenny <gtg287y@gmail.com>
2017-09-05 10:50:02 -0400

    scout/guide notes

--- web/scripts/GameEntities/ClasspectStuff/Classes/Guide.dart
+++ web/scripts/GameEntities/ClasspectStuff/Classes/Guide.dart
@@ -2,7 +2,7 @@ import "SBURBClass.dart";
 
 class Guide extends SBURBClass {
     List<String> handles = <String>["guiding", "gracious", "great", "gratuitous", "greeting", "gloved", "gone"];
-
+//i am thinking guides will give other players their own aspects (and not the guides) while scouts will gain whoever they are with's aspect.
 
     Guide() : super("Guide", 16, false);
 
