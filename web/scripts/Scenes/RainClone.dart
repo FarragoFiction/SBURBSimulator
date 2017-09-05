36668caaa01f9ae80c1c32138adb4fb8db546b10
aCactusOrSomething <31144053+aCactusOrSomething@users.noreply.github.com>
2017-09-04 19:30:51 -0500

    fixes

--- web/scripts/Scenes/RainClone.dart
+++ web/scripts/Scenes/RainClone.dart
@@ -5,19 +5,20 @@ import "../SBURBSim.dart";
 class RainClone extends Scene {
   List<Player> playerList = []; //what players are already in the medium when i trigger?
   bool canRepeat = false; //todo: change such that this can repeat 2-3 times in
-  Player player = null;
+  Player original = null;
+  Player other = null;
 
   RainClone(Session session) :super(session);
 
   @override
   bool trigger(List<Player> playerList){
     this.playerList = playerList;
-    var living = findLivingPlayers(this.session.players);
-    this.player = findAspectPlayer(living, Aspects.RAIN);
-    if (this.player == null) {
+    var living = findLivingPlayers(playerList);
+    this.original = findAspectPlayer(living, Aspects.RAIN);
+    if (this.original == null) {
       return false;
     } else {
-      return (this.player != null);
+      return (this.original != null);
     }
   }
   @override
@@ -28,34 +29,36 @@ class RainClone extends Scene {
     appendHtml(div,canvasHTML);
 
     var canvas = querySelector("#canvas"+ divID);
-    Drawing.drawGodSymbolBG(canvas, this.player);
-    Drawing.drawSprite(canvas, this.player);
-    Drawing.drawSpriteTurnways(canvas, this.player);
+    Drawing.drawGodSymbolBG(canvas, this.original);
+    Drawing.drawSprite(canvas, this.original);
+    Drawing.drawSpriteTurnways(canvas, this.original);
 
-    String ret = "The " + this.player.htmlTitle() + " Just made a clone of themselves?";
+    String ret = "The " + this.original.htmlTitle() + " Just made a clone of themselves?";
     appendHtml(div, ret);
 
     //todo: make it hapen.
     Player rainyClone = new Player();
-    rainyClone = this.player.clone();
-    rainyClone.session = this.player.session;
-    rainyClone.denizen = this.player.denizen;//clones don't get their own denizen. hopefully, this wont exile the Original's Denizen...
-    rainyClone.relationships = this.player.relationships;//todo:
+    rainyClone = this.original.clone();
+    rainyClone.session = this.original.session;
+    rainyClone.denizen = this.original.denizen;//clones don't get their own denizen. hopefully, this wont exile the Original's Denizen...
+    rainyClone.relationships = this.original.relationships;//todo:
 
 
-    Relationship.transferFeelingsToClones(this.player, [rainyClone]);
+    Relationship.transferFeelingsToClones(this.original, [rainyClone]);
     for (int j = 0; j < this.session.players.length; j++) {
-      this.player = this.session.players[j];
-      player.generateRelationships([rainyClone]);
+      this.other = this.session.players[j];
+      this.other.generateRelationships([rainyClone]);
+      rainyClone.generateRelationships([this.other]);
     } //should have neutral relationship with self.
 
-    rainyClone.guardian = this.player;
-    for (int i = 0; i < playerList.length; i++) {
-      GameEntity g = playerList[i]; //could be a sprite, and they don't have classpects.
+    rainyClone.guardian = this.original.guardian;
+    for (int i = 0; i < this.session.players.length; i++) {
+      GameEntity g = this.session.players[i]; //could be a sprite, and they don't have classpects.
       if (g is Player) {
-        Player p = playerList[i];
-        if (p == this.player) {
-          super.session.players.insert(i, rainyClone);
+        Player p = this.session.players[i];
+        if (p == this.original) {
+          super.session.players.insert(i + 1, rainyClone);
+          i = this.session.players.length + 1;
         }
       }
     }
