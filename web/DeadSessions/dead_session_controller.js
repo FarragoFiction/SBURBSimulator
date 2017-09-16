a792a06d9f3ae100c71bd304b5ae8dfc6dcca3a9
Jenny <gtg287y@gmail.com>
2017-09-16 11:02:17 -0400

    preparing dead sessions

--- web/DeadSessions/dead_session_controller.js
+++
@@ -1,6 +0,0 @@
-window.onload = function() {
-    console.log("???")
-    //this is clearly now how you use dart objects in javascript
-    var p = new Pet("Dog", "chuff", "Tina");
-     p.makeSound();
-}
---
+++ web/scripts/Controllers/DeadStoryController.dart
@@ -0,0 +1,43 @@
+import '../SBURBSim.dart';
+import '../navbar.dart';
+import 'dart:async';
+import 'dart:html';
+import 'dart:typed_data';
+import 'dart:collection';
+
+//replaces the poorly named scenario_controller2.js
+void main() {
+  //maybe if i define it here it won't be the same as end time
+  startTime =new DateTime.now();
+  //print("If you are in dartium, make sure to select this file to access it's global vars");
+  new DateTime.now();
+  new Timer(new Duration(milliseconds: 1000), () =>window.scrollTo(0, 0));
+
+  //make a new StoryController (which will auto set itself as it's parent's singleton instance
+  window.onError.listen((Event event){
+  	ErrorEvent e = event as ErrorEvent;
+    //String msg, String url, lineNo, columnNo, error
+    printCorruptionMessage(e);//(e.message, e.path.toString(), e.lineno.toString(), e.colno.toString(), e.toString());
+    return;
+  });
+  loadNavbar();
+  new DeadStoryController(); //will set this as SimController's instance variable.
+  if(getParameterByName("seed",null) != null){
+   // Math.seed = getParameterByName("seed");  //TODO replace this somehow
+    SimController.instance.initial_seed = int.parse(getParameterByName("seed",null));
+  }else{
+    var tmp = getRandomSeed();
+   // Math.seed = tmp; //TOdo do something else here but rand is inside of session......
+    SimController.instance.initial_seed = tmp;
+  }
+
+  SimController.instance.shareableURL();
+
+  SimController.instance.startSession();
+}
+
+
+//TODO: figure out how to have this make a DeadSession instead of a regular one.
+class DeadStoryController extends SimController {
+  DeadStoryController() : super();
+}
