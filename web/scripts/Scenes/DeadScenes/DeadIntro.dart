5958241343bcd2465884d1a795433d212c057728
Jenny <gtg287y@gmail.com>
2017-09-17 21:22:13 -0400

    newspost

--- web/scripts/Scenes/DeadScenes/DeadIntro.dart
+++ web/scripts/Scenes/DeadScenes/DeadIntro.dart
@@ -14,9 +14,10 @@ class DeadIntro extends Scene {
         (session as DeadSession).makeDeadLand();
         String divID = "deadIntro${session.players[0].id}";
         String narration = "A wave of destruction heralds the arrival of the ${player.htmlTitle()}. They are the only Player. SBURB was never meant to be single player, and they have activated the secret 'Dead Session' mode as a punishment. Or is it a reward?  ";
-        narration += " Skaia is black and lifeless. The Land itself is merely the ${player.htmlTitle()}'s now barren home world, dragged kicking and screaming into the session. ";
+        narration += " <Br><br>Skaia is black and lifeless. ";
         narration += "What can they even do now? Is there even a way to win? ";
-        narration += " They stare hopelessly at what was their former planet, now transformed into the ${player.landFuture.name}. ";
+        narration += " <Br><Br>They stare hopelessly at what was their former planet, now transformed into the ${player.landFuture.name}. ";
+        narration += "<HR><h2>HEY, JR HERE. THIS IS A WORK IN PROGRESS THAT IS <B>GOING</B> TO CRASH. DEAL WITH IT. I'M STILL CODING IT.</h2><hr>";
         narration += " ${player.landFuture.randomFlavorText(session.rand, player)} ";
         String html = "<canvas id='${divID}' width='${canvasWidth.toString()}' height='${canvasHeight.toString()}'>  </canvas><br><Br>$narration";
         appendHtml(div, html);
