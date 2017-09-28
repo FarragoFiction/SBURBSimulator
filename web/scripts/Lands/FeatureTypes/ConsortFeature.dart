88b683307c995970a4b36ae9afd0d30959e1a319
Jenny <gtg287y@gmail.com>
2017-09-28 15:57:49 -0400

    there we go. now just gotta debug why it crashes after dinner

--- web/scripts/Lands/FeatureTypes/ConsortFeature.dart
+++ web/scripts/Lands/FeatureTypes/ConsortFeature.dart
@@ -29,12 +29,6 @@ class ConsortFeature extends Feature {
 }
 
 class CarapaceFeature extends ConsortFeature {
-    /// what are these consorts called?
-    String name;
-    ///what sound do these consorts make?
-    String sound;
-    ///TODO eventually have an actual npc object associated here, to generate npc helpers
-    ///TODO eventually have like a color for npcs, once we have the rendering engine. maybe the FeatureFactory version of a consort is generic, but when given to a land a random color is rolled.
     CarapaceFeature(String name, String sound): super(name, sound);
 
     Carapace makeCarapace(Session s) {
