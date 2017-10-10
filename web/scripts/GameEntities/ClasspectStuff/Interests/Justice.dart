58cbe7a91f56e81d2331dbdf668e453cbbc2d3c1
Jenny <gtg287y@gmail.com>
2017-10-10 12:20:07 -0400

    justice ready to test

--- web/scripts/GameEntities/ClasspectStuff/Interests/Justice.dart
+++ web/scripts/GameEntities/ClasspectStuff/Interests/Justice.dart
@@ -57,9 +57,9 @@ class Justice extends InterestCategory {
             ..addFeature(FeatureFactory.DECEITSMELL, Feature.HIGH)
             ..addFeature(FeatureFactory.FRANTICFEELING, Feature.MEDIUM)
             ..addFeature(new PreDenizenQuestChain("Start a Rebellion", [
-                new Quest("The ${Quest.PLAYER1} finds a group of three ${Quest.CONSORT}s ${Quest.CONSORTSOUND}ing about how the Prospitian government SEEMS nice and friendly, but it's all a cover up!  Intrigued, the ${Quest.PLAYER1} learns of several conspiracies they have surpressed, not LEAST of which is the existance of ALIENS in the Medium. "),
-                new Quest("The ${Quest.PLAYER1} does a daring mission at a local Prospitian embassy and uncovers all sorts of juicy secrets. They can't help but read some of them ahead of time and- oh. Oh I see. In retrospect, the players kind of ARE aliens to the Medium. This conspiracy is a lot less interesting now.  "),
-                new Quest("The ${Quest.PLAYER1} shows the conspiracy papers to the three ${Quest.CONSORT}s, who fail to see the humor in the situation. They immediately try to rally their fellow ${Quest.CONSORT}s against the alien threat that is the foretold Players themselves. They...don't get even a single solitary ${Quest.CONSORTSOUND} of agreement from the crowd, though. Looks like they are stuck being the wacky conspiracy theorists. "),
+                new Quest("The ${Quest.PLAYER1} finds a crowd of ${Quest.CONSORTSOUND}ing ${Quest.CONSORT}s. They are holding signs with slogans like 'This isn't Fair' and 'Don't be Jerks'. Apparently they have a problem with the upper class ${Quest.CONSORT}s in charge. The ${Quest.PLAYER1} is moved by their plight and agrees to try to help."),
+                new Quest("The ${Quest.PLAYER1} meets with the upper class ${Quest.CONSORT}s to try to negotiate a peaceful revolution. Unfortunately, the ${Quest.CONSORT}s refuse to listen to reason, and even call their guards to attack the ${Quest.PLAYER1}. After easily defeating the guards, the ${Quest.PLAYER1} declares war. You cannot stop the fires of Revolution!  "),
+                new Quest("It has been a long struggle, but finally the corrupt high class ${Quest.CONSORT}s have been taken prisoner. The common ${Quest.CONSORT}s ${Quest.CONSORTSOUND} and rejoice and declare a national holiday. The rebellion has won!"),
             ], new FraymotifReward(), QuestChainFeature.defaultOption), Feature.WAY_LOW)
             ,  Theme.LOW);
     }
