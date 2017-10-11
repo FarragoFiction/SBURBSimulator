69f23db65358159edc5ac19c69d8df44baf20a95
Jenny <gtg287y@gmail.com>
2017-10-11 11:40:33 -0400

    all social quests ready to test

--- web/scripts/GameEntities/ClasspectStuff/Interests/Social.dart
+++ web/scripts/GameEntities/ClasspectStuff/Interests/Social.dart
@@ -37,18 +37,18 @@ class Social extends InterestCategory {
             ], new FraymotifReward(), QuestChainFeature.defaultOption), Feature.WAY_LOW)
             ,  Theme.LOW);
 
-        addTheme(new Theme(<String>["Meditation","Worship","Altars","Hymns", "Chapels", "Priests", "Angels", "Religion"])
+        addTheme(new Theme(<String>["Meditation","Chants","Worship","Altars","Hymns", "Chapels", "Priests", "Angels", "Religion"])
             ..addFeature(FeatureFactory.SILENCE, Feature.LOW)
-            ..addFeature(FeatureFactory.CALMFEELING, Feature.MEDIUM)
+            ..addFeature(FeatureFactory.CALMFEELING, Feature.HIGH)
             ..addFeature(FeatureFactory.CONTEMPLATATIVEFEELING, Feature.HIGH)
             ..addFeature(FeatureFactory.CHANTINGSOUND, Feature.HIGH)
             ..addFeature(FeatureFactory.MUSICSOUND, Feature.HIGH)
             ..addFeature(FeatureFactory.SINGINGSOUND, Feature.HIGH)
             ..addFeature(FeatureFactory.STUDIOUSFEELING, Feature.MEDIUM)
-            ..addFeature(new PreDenizenQuestChain("Learn About Frogism", [
-                new Quest("The ${Quest.PLAYER1} "),
-                new Quest("The ${Quest.PLAYER1}    "),
-                new Quest(" The ${Quest.PLAYER1} ")
+            ..addFeature(new PreDenizenQuestChain("Meditate On Frogism", [
+                new Quest("The ${Quest.PLAYER1} wonders into an incredibly calm area of their land. It is filled with chanting and ${Quest.CONSORTSOUND}ing ${Quest.CONSORT}s. Apparently they are monks contemplating the vastness of the Vast Croak. The ${Quest.PLAYER1} joins them. "),
+                new Quest("While meditating with the ${Quest.CONSORT} monks, the ${Quest.PLAYER1} has come to a startling realization, the vast croak is related to ${Quest.MCGUFFIN}-ness. How could they miss this?"),
+                new Quest(" The ${Quest.PLAYER1} begins telling all the ${Quest.CONSORT} monks about how ${Quest.MCGUFFIN}-ness relates to the Vast Croak. The monks begin ${Quest.CONSORTSOUND}ing in amazement, this could revolutionize Frogism for generations!")
             ], new FraymotifReward(), QuestChainFeature.defaultOption), Feature.WAY_LOW)
             ,  Theme.LOW);
 
@@ -60,9 +60,9 @@ class Social extends InterestCategory {
             ..addFeature(FeatureFactory.HEROICFEELING, Feature.HIGH)
             ..addFeature(FeatureFactory.DRAGONCONSORT, Feature.HIGH)
             ..addFeature(new PreDenizenQuestChain("Steal the Door", [
-                new Quest("The ${Quest.PLAYER1} "),
-                new Quest("The ${Quest.PLAYER1}    "),
-                new Quest(" The ${Quest.PLAYER1} ")
+                new Quest("The ${Quest.PLAYER1} wanders into an entire crowd of ${Quest.CONSORT}s dressed in authentic style fantasy armor. What is going on? The nearest one stops ${Quest.CONSORTSOUND}ing long enough to explain that it's time for Paradox Space's biggest FLARP convention. The ${Quest.PLAYER1} happily joins the rp."),
+                new Quest("The ${Quest.PLAYER1}'s character in the FLARP is nearly maximum level. This is so much fun! Suddenly, a group of underlings attack the crowd. Confusion reigns as the ${Quest.CONSORT}s think it's somehow related to FLARP at first and don't fight back seriously. It is up to the ${Quest.PLAYER1} to save the day! All that RP practice surprisingly pays off. They win easily!   "),
+                new Quest("The FLARP is finally coming to an end. All the ${Quest.CONSORT}s agree that it is the best session in living memory and celebrate how the ${Quest.PLAYER1} kept it from becoming a tragedy.")
             ], new FraymotifReward(), QuestChainFeature.defaultOption), Feature.WAY_LOW)
             ,  Theme.LOW);
     }
