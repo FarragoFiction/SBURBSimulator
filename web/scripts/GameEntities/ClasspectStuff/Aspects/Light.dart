dac73756ac5feb533d51cbb25ee633c6ca43f0a6
Jenny <gtg287y@gmail.com>
2017-10-17 14:28:33 -0400

    one theme remains for Light quests

--- web/scripts/GameEntities/ClasspectStuff/Aspects/Light.dart
+++ web/scripts/GameEntities/ClasspectStuff/Aspects/Light.dart
@@ -85,21 +85,24 @@ class Light extends Aspect {
             ..addFeature(FeatureFactory.GLAMOROUSFEELING, Feature.HIGH)
             ..addFeature(FeatureFactory.JAZZSOUND, Feature.HIGH) //persona5 has changed the face of casinos for me for all time
             ..addFeature(new DenizenQuestChain("Go Big or Go Home", [
-                new Quest("The ${Quest.PLAYER1} finds a sparkling Casino. Inside, amid ${Quest.CONSORTSOUND}ing ${Quest.CONSORT}s is a single door, locked by three runes, each depicting a different form of gambling. Huh. The ${Quest.PLAYER1} approaches the slots and begins to play. Finally, after what must be hours, they get three Light symbols. The Slot rune lights up. One down, two to go."),
+                new Quest("The ${Quest.PLAYER1} finds a sparkling Casino. Inside, amid ${Quest.CONSORTSOUND}ing ${Quest.CONSORT}s is a single door, locked by three runes, each depicting a different form of gambling, Slots, Cards and Coins. Huh. The ${Quest.PLAYER1} approaches the slots and begins to play. Finally, after what must be hours, they get three Light symbols. The Slot rune lights up. One down, two to go."),
                 new Quest("The ${Quest.PLAYER1} thinks they finally have the rules of poker down enough to try the next set of gambling challenges. After a nerve wracking series of hands, they bet it all on a risky gamble, only to pull through with a Royal Flush!  The Cards rune lights up."),
                 new Quest("It is time for the final gamble. A single coin flip will decide it all. No take backs, no replays.   It lands. TAILS! The Coin rune lights up, and the door is open. The ${Quest.PLAYER1} begins to prepare for whatever may lay within."),
-                new DenizenFightQuest("","","")
+                new DenizenFightQuest("When the ${Quest.PLAYER1} finally enters the Casino Door, the ${Quest.DENIZEN} is already rampaging. They are pissed off as fuck that they have been off screen this entire time, and blame the ${Quest.PLAYER1} for it. It's time to strife! ","The ${Quest.PLAYER1} was lucky enough to pull off a win! The ${Quest.DENIZEN} is too dead too rampage.","Bad break, the ${Quest.DENIZEN} is going to keep throwing a hissy fit.")
             ], new DenizenReward(), QuestChainFeature.defaultOption), Feature.WAY_LOW)
             ,  Theme.HIGH);
         addTheme(new Theme(<String>["Glow", "Light", "Rays","Sun", "Shine", "Sparkle", "Sunshine","Stars" ])
             ..addFeature(FeatureFactory.NATURESMELL, Feature.MEDIUM)
             ..addFeature(FeatureFactory.CALMFEELING, Feature.MEDIUM)
             ..addFeature(FeatureFactory.HAPPYFEELING, Feature.HIGH)
+            ..addFeature(FeatureFactory.LIZARDCONSORT, Feature.HIGH)
+            ..addFeature(FeatureFactory.IGUANACONSORT, Feature.HIGH)
+            ..addFeature(FeatureFactory.CHAMELEONCONSORT, Feature.HIGH)
             ..addFeature(new DenizenQuestChain("Shine the Light", [
-                new Quest(""),
-                new Quest(""),
-                new Quest(""),
-                new DenizenFightQuest("","","")
+                new Quest("The ${Quest.PLAYER1} finds an incongruous dark patch in the otherwise brightly lit land. A quivering ${Quest.CONSORT} explains that the ${Quest.DENIZEN} has forbidden the ${Quest.CONSORT}s from having light, and moved giant disks to block it from them. Now that's just being mean!  The ${Quest.PLAYER1} vows to help.   "),
+                new Quest("The ${Quest.PLAYER1} has finally managed to destroy the disk blocking light from the ${Quest.CONSORT} village. There is a chorus of happy ${Quest.CONSORTSOUND}s as they bask in the light. The ${Quest.PLAYER1} feels good about a job well done. "),
+                new Quest("Disaster!  The ${Quest.PLAYER1} village is once again shrouded in darkness, this time from an even larger disk than before. Judging from the roars, the ${Quest.DENIZEN} is guarding the disk themself.  The ${Quest.PLAYER1} must prepare themself for a tought fight.  "),
+                new DenizenFightQuest("The ${Quest.PLAYER1} has managed to reach the disk guarded by the ${Quest.DENIZEN}. The monster denies even so basic a right as light, there can be no quarter. It's time to stife!","The ${Quest.DENIZEN} is defeated, the disk destroyed. Finally, the ${Quest.CONSORT}s can enjoy the light in peace.","Darkness reigns.")
             ], new DenizenReward(), QuestChainFeature.defaultOption), Feature.WAY_LOW)
 
             , Theme.HIGH);
@@ -111,10 +114,10 @@ class Light extends Aspect {
             ..addFeature(FeatureFactory.CALMFEELING, Feature.MEDIUM)
             ..addFeature(FeatureFactory.STUDIOUSFEELING, Feature.HIGH)
             ..addFeature(new DenizenQuestChain("Shed the Light", [
-                new Quest(""),
-                new Quest(""),
-                new Quest(""),
-                new DenizenFightQuest("","","")
+                new Quest("${Quest.CONSORT}s are falling ill left and right due to a mysterious Plague. It doesn't seem to follow the pattern of a disease, having no communicability. What is going on and how can the ${Quest.PLAYER1} possibly stop it?"),
+                new Quest("The ${Quest.PLAYER1} pours over maps and charts. What do the sick ${Quest.CONSORT}s have in common with each other?  Finally, there is a flash of inspiration. ${Quest.CONSORT}s who live or work closer to a particular river that meanders across the planet are the ones becoming ill! The ${Quest.PLAYER1} quickly orders the river quaranteend and new cases begin to taper off. Now it remains to see what can be done about those already sick. "),
+                new Quest("The ${Quest.PLAYER1} analyzes the water from the contaminated well. Boiling it reveals a thick black sludge.  The boiled water is found to be perfectly safe to drink. The ${Quest.PLAYER1} discovers that the sludge causes a strange vitamin deficiency, and supplies the town with supplements to fix the sickness.  Things are looking brighter, indeed."),
+                new DenizenFightQuest("The ${Quest.PLAYER1} traces the contaminated river back to it's source. The ${Quest.PLAYER1} is revealed to be dripping the gross as fuck sludge into the river. When negotiations fail to make it leave the water, the only remaining option is Stife. ","The ${Quest.PLAYER1} is victorious. The ${Quest.DENIZEN}'s body despawns, along with all the sludge in the river. The Plague is finally over!","The darkness of the Plague remains.")
             ], new DenizenReward(), QuestChainFeature.defaultOption), Feature.WAY_LOW)
             , Theme.HIGH); // end theme
 
