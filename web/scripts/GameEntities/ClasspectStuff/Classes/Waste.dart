4d94011f29f2f5f7021fc1a39d5e36c8207693e7
Jenny <gtg287y@gmail.com>
2017-10-25 08:31:09 -0400

    wastes ready to test

--- web/scripts/GameEntities/ClasspectStuff/Classes/Waste.dart
+++ web/scripts/GameEntities/ClasspectStuff/Classes/Waste.dart
@@ -75,18 +75,34 @@ class Waste extends SBURBClass {
             ..addFeature(FeatureFactory.CONFUSINGFEELING, Feature.HIGH)
             ..addFeature(FeatureFactory.NOTHINGSMELL, Feature.MEDIUM)
 
+            ..addFeature(new PreDenizenQuestChain("Be Spooked By a Wolf", [
+                new Quest("The ${Quest.PLAYER1} is trapped in an attic. Bullies chased them here. AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH!  Oh god, that Wolf Head is terrifying!"),
+                new Quest("QUITE FRANKLY, your majesty, I don't think you realize what kind of hell the ${Quest.PLAYER1} been through. Do you have even the SLIGHTEST CLUE how many times that wolf head over there has SCARED THE SHIT OUT OF THEM???"),
+                new Quest("Fuck. The ${Quest.PLAYER1} is so upset that you don't understand how scary that Spooky Wolf is that they've started babbling about different forms of fictional romance. Welp. Nothing to see here. We better just skip this. ")
+            ], new Reward(), QuestChainFeature.defaultOption), Feature.WAY_LOW) //not guaranteed like other quests are
+            ..addFeature(new DenizenQuestChain("Be The Illegitimate Player", [
+                new Quest("A wizened ${Quest.CONSORT} explains the rules of some convoluted, boring-ass puzzle to the ${Quest.PLAYER1}. Wait wait wait, did they just say something about 'no legitimate way to meet ${Quest.DENIZEN}'?  Hell FUCKING yes, that means there's some ILLEGITIMATE way. "),
+                new Quest("After way too much obsessive focus, the ${Quest.PLAYER1} thinks they are onto something. This shitty game is just code, right?  There must be some glitch or exploit or out-right fucking HACK to get to the secret content.  They are gonna meet the FUCK out of ${Quest.DENIZEN}."),
+                new Quest("Hell FUCKING yes!!! The ${Quest.PLAYER1} has bugged and fussed and meddled with the code until they are standing in front of ${Quest.DENIZEN}. After solving some bullshit extra bonus Riddle, they gain access to The Hoarde. ")]
+                , new DenizenReward(), QuestChainFeature.defaultOption), Feature.WAY_HIGH)
+
             ..addFeature(new DenizenQuestChain("Wear the Merch, Be the Rider", [
                 new Quest("The ${Quest.PLAYER1} is sick and tired of being bullied!  If only there was some way they could finally defeat those mean old bullies.   A wizened ${Quest.CONSORT} tells of a legendary artifact that could-- Wait. No. That's so boring.   The ${Quest.PLAYER1} decides to update their highly-indulgent meta work instead."),
                 new Quest("Holy shit, did you know you could alchemize MERCHANDISE of your highly indulgent meta work? The ${Quest.PLAYER1} is just covered in merch now. It's great."),
                 new Quest("Holy fuck! It turns out that the ${Quest.DENIZEN} is a fan of the ${Quest.PLAYER1}'s highly indulgent meta work!  They also agreed to be called 'Falcor', because, come ON that was a great movie!  The ${Quest.PLAYER1} hops up onto Falcor's back and this is the single coolest thing that has ever happened in all of Paradox Space."),
                 new Quest("With a dramatic 'BORF' the bullies are defeated by Falcor! And so came to an end the most heroic thing that ever happened in the history of metafiction. <br><br>Let's move on.")
             ], new DenizenReward(), QuestChainFeature.defaultOption), Feature.WAY_HIGH)
-            ..addFeature(new PostDenizenQuestChain("Purify the Water", [
-                new Quest("The defeat of the ${Quest.DENIZEN} has some unforseen consequences, including the tainting of the water for the majority of the land. The ${Quest.CONSORT}s seem to have figured out a solution in the short term, but the ${Quest.PLAYER1} resolves to bug and fuss and meddle until things are fixed the right way. "),
-                new Quest("So far, the ${Quest.PLAYER1} hasn't had much luck getting ${Quest.CONSORT}s to build a water purifying facility.  They are content just boiling their water. 'It's not hard', they say. It's so frustrating that the ${Quest.PLAYER1} knows they can help them but the ${Quest.CONSORT}s just will NOT cooperate. "),
-                new Quest("The ${Quest.PLAYER1} has finally accepted that some people just don't want to be helped. As they make peace with this, a mysterious glow emerges from their chest.  The water of the land matches this glow, and the water is purified through the power of ${Quest.MCGUFFIN}. Huh. Okay then.")
-            ], new BoonieFraymotifReward(), QuestChainFeature.defaultOption), Feature.WAY_LOW)
-            ,  Theme.MEDIUM);
+            ..addFeature(new PostDenizenQuestChain("Die Ironically, In The Proximity Of Some Horses", [
+                new Quest("A wizened ${Quest.CONSORT} tells the ${Quest.PLAYER1} that they are going to die. Ironically.   In the proximity of some horses.  The ${Quest.PLAYER1} shrugs and keeps updating their highly indulgent meta work. "),
+                new Quest("Glowing letters, three stories tall,  lit by flame, heavy with the weight of prophecy proclaim 'You Are Going To Die. Ironically.   In the Proximity Of Some Horses'. The ${Quest.PLAYER1} wonders if it's like, a metaphor or something?"),
+                new Quest("In a scene predicted by no one, the ${Quest.PLAYER1} dies. In the proximity some horses. How ironic, that their very demise would be in the proximity of some horses. What? You didn't follow that? Just think it over. Think it over...  Luckily being dead doesn't seem to affect the ${Quest.PLAYER1}'s narrative importance at all.   Hell, are you sure they weren't dead all along? ")//hussie has white ghost eyes the whole session, after all.
+            ], new BoonieFraymotifReward(), QuestChainFeature.defaultOption), Feature.WAY_HIGH)
+            ..addFeature(new PostDenizenQuestChain("Run The Simulations", [
+                new Quest("Huh. The ${Quest.PLAYER1} has figured out how to run simulations of SBURB? What is even the point? Man, it's a fucking Waste. Maybe there IS no point??? "),
+                new Quest("Okay, revised statement: maybe the point of running simulations is to map out all of Paradox Space? Makes way more sense than just having a big black sheet of paper, right? The ${Quest.PLAYER1} makes a robot doppelganger to go explore areas of Paradox Space that are predicted to have useful features. Huh, looks like it's working!"),
+                new Quest("Welp. Whatever original reason the ${Quest.PLAYER1} had for finding other sessions has fallen by the wayside. They've gotten completely distracted helping out sessions with no alpha and accidentally dooming the fuck out of everyone when they make a typo in some code.   I thought these were just simulations? Fuck Paradox Space. ")
+            ], new BoonieFraymotifReward(), QuestChainFeature.defaultOption), Feature.WAY_HIGH)
+            ,  Theme.HIGH);
     }
 
 
