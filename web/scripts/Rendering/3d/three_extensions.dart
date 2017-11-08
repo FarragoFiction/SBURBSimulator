0a763ad246790d4c6a4f2643bb1dc682d8eebc4f
Unknown <emperorlexx@hotmail.com>
2017-11-08 06:47:34 +0000

    Font triangulation experiments

--- web/scripts/Rendering/3d/three_extensions.dart
+++ web/scripts/Rendering/3d/three_extensions.dart
@@ -4,6 +4,7 @@ import "package:js/js_util.dart" as JsUtil;
 
 import '../../includes/colour.dart';
 import "../../loader/loader.dart";
+import "../text/opentype.dart" as OT;
 import "three.dart";
 
 Future<ShaderMaterial> makeShaderMaterial(String vertexfile, String fragmentfile) async {
@@ -25,5 +26,36 @@ void setUniform(ShaderMaterial mat, String name, ShaderUniform<dynamic> value) {
 
 Vector4 colour2vec(Colour colour) {
     return new Vector4(colour.redDouble, colour.greenDouble, colour.blueDouble, colour.alphaDouble);
+}
+
+Shape getShapeForText(String text, OT.Font font, num fontSize) {
+    OT.Path textPath = font.getPath(text, 0, 0, fontSize);
+
+    return getShapeFromOTPath(textPath);
+}
+
+Shape getShapeFromOTPath(OT.Path path) {
+    Shape shape = new Shape();
+
+    for(OT.PathCommand c in path.commands) {
+        if (c.type == "M") {
+            print("moveTo ${c.x},${c.y}");
+            shape.moveTo(c.x, c.y);
+        } else if (c.type == "L") {
+            print("lineTo ${c.x},${c.y}");
+            shape.lineTo(c.x, c.y);
+        } else if (c.type == "Q") {
+            print("quadraticCurveTo ${c.x},${c.y}, control: ${c.x1},${c.y1}");
+            shape.quadraticCurveTo(c.x1, c.y1, c.x, c.y);
+        } else if (c.type == "C") {
+            print("bezierCurveTo ${c.x},${c.y}, controls: ${c.x1},${c.y1}, ${c.x2},${c.y2}");
+            shape.bezierCurveTo(c.x1, c.y1, c.x2, c.y2, c.x, c.y);
+        } else if (c.type == "Z") {
+            print("closePath");
+            shape.closePath();
+        }
+    }
+
+    return shape;
 }
 
