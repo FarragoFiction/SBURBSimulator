6e785039d2c65372416a007b54f8daddc88c2b4c
Jenny <gtg287y@gmail.com>
2017-11-17 13:30:43 -0500

    simple template for alchemy mini game set up, need to wire shit up

--- web/scripts/Controllers/Misc/AlchemyController.dart
+++ web/scripts/Controllers/Misc/AlchemyController.dart
@@ -4,29 +4,31 @@ import '../../navbar.dart';
 
 Player player;
 Element storyDiv;
+Element item1Div;
+Element item2Div;
+Element operatorDiv;
+Element resultDiv;
 
-/*
-     TODO:
-     * 3 drop downs, all items, alchemy function, all items again.
-     * stats for 2 items (list of traits plus rank of total item)
-     * button to do alchemy
-        * displays new item stats
-        * new item added to list of all items.
-    * Grist hoard. Alchemy has grist cost (how?)
-    * Achievements (think little inferno).
- */
+Element item1TraitsDiv;
+Element item2TraitsDiv;
+Element resultTraitsDiv;
 
 void main() {
-    loadNavbar();
+    //loadNavbar();
     globalInit();
     init();
 }
 
 void init() {
     storyDiv = querySelector("#story");
-    player = randomPlayer(new Session(13));
+    item1Div = querySelector("#item1");
+    operatorDiv = querySelector("#operator");
+    item2Div = querySelector("#item2");
+    resultDiv = querySelector("#result");
+    player = randomPlayer(new Session(int.parse(todayToSession())));
     populateSylladex();
     makeDropDowns();
+    makeStatsDisplay();
 }
 
 void populateSylladex() {
@@ -35,10 +37,18 @@ void populateSylladex() {
     }
 }
 
+void makeStatsDisplay() {
+    Item item = player.sylladex.first;
+    item1TraitsDiv = (renderItemStats(item));
+    item1Div.append(item1TraitsDiv);
+    item2TraitsDiv = (renderItemStats(item));
+    item2Div.append(item2TraitsDiv);
+}
+
 void makeDropDowns() {
-    SelectElement firstItem = genericDropDown(storyDiv, player.sylladex,  "First Item");
-    SelectElement operator = genericDropDown(storyDiv, <String>["And","Or","Xor"],  "Operation");
-    SelectElement secondItem = genericDropDown(storyDiv, player.sylladex,  "Second Item");
+    SelectElement firstItem = genericDropDown(item1Div, player.sylladex,  "First Item");
+    SelectElement operator = genericDropDown(operatorDiv, <String>["And","Or","Xor"],  "Operation");
+    SelectElement secondItem = genericDropDown(item2Div, player.sylladex,  "Second Item");
 }
 
 SelectElement genericDropDown<T> (Element div, List<T> list, String name)
@@ -57,4 +67,29 @@ SelectElement genericDropDown<T> (Element div, List<T> list, String name)
     return selector;
 }
 
+
+Element renderItemStats(Item item) {
+    Element ret = new DivElement();
+    ret.classes.add("itemStats");
+    Element header = new DivElement();
+    header.setInnerHtml(item.fullName);
+    header.classes.add("itemHeader");
+
+    ret.append(header);
+    Element rank = new DivElement();
+    rank.setInnerHtml("Rank: ${item.rank}");
+    ret.append(rank);
+
+    Element attributes = new DivElement();
+    attributes.setInnerHtml("Attributes: ");
+    ret.append(attributes);
+
+    for(ItemTrait it in item.traits) {
+        Element li = new DivElement();
+        li.classes.add("oneTrait");
+        li.setInnerHtml(it.descriptions.first);
+        ret.append(li);
+    }
+    return ret;
+}
 
