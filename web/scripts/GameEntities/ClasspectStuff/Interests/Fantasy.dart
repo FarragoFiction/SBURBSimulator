11135677b8e14c660d7453fd4f24b1e0b4be4850
Jenny <gtg287y@gmail.com>
2018-01-25 23:40:45 -0500

    tweaking quests

--- web/scripts/GameEntities/ClasspectStuff/Interests/Fantasy.dart
+++ web/scripts/GameEntities/ClasspectStuff/Interests/Fantasy.dart
@@ -51,7 +51,7 @@ class Fantasy extends InterestCategory {
                 new Quest("The ${Quest.PLAYER1} learns of a Beautiful ${Quest.CONSORT} who has been kidnapped by the vial ${Quest.DENIZEN} Minion. There is a hefty reward should a brave Hero rescue them. "),
                 new Quest(" The ${Quest.PLAYER1} journeys to the castle of the ${Quest.DENIZEN} Minion, only to discover that they way is barred by a giant iron lock. Only the correctly shaped ${Quest.PHYSICALMCGUFFIN} can open it. Looks like it's time to go questing."),
                 new Quest("The ${Quest.PLAYER1} finally finds a correctly shaped.  ${Quest.PHYSICALMCGUFFIN}.  The gate swings open, and the ${Quest.DENIZEN} Minion is swiftly defeated. The Beautiful ${Quest.CONSORT} delivers a reward to the brave ${Quest.PLAYER1}. Convenient that they had it with them when they were kidnapped, huh?   "),
-            ], new ConsortReward(), QuestChainFeature.defaultOption), Feature.WAY_LOW)
+            ], new RandomReward(), QuestChainFeature.defaultOption), Feature.WAY_LOW)
             ,  Theme.LOW);
 
 
@@ -66,7 +66,7 @@ class Fantasy extends InterestCategory {
                 new Quest("The ${Quest.PLAYER1} recieves an invitation to dine at the remote castle of Count Feratu. No one ever sees this mysterious ${Quest.CONSORT} leave, and no one can remember the last time he received guests."),
                 new Quest("The ${Quest.PLAYER1} attends the dinner. Count Feratu is an.... eccentric ${Quest.CONSORT}. He sure does like drinking dark red liquids! And being in dimly lit rooms. And telling you to ignore the blatant screams coming from the dungeon. Luckily it all turns out to be a wacky misunderstanding. Really, VAMPIRES are fakey fake bullshit."),
                 new Quest(" The ${Quest.PLAYER1} sees Castle Feratu in the distance. The silhouette of a ${Quest.CONSORT} standing on a balcony suddenly morphs into a bat and flies away. Holy shit, maybe he really WAS a vampire!? But...he doesn't seem to be hurting anyone, so...live and let live, you guess. "),
-            ], new ConsortReward(), QuestChainFeature.defaultOption), Feature.WAY_LOW)
+            ], new RandomReward(), QuestChainFeature.defaultOption), Feature.WAY_LOW)
             ,  Theme.LOW);
 
         //the sock ruse was a distaction
@@ -81,7 +81,7 @@ class Fantasy extends InterestCategory {
                 new Quest("The ${Quest.PLAYER1} finds a group of three ${Quest.CONSORT}s ${Quest.CONSORTSOUND}ing about how the Prospitian government SEEMS nice and friendly, but it's all a cover up!  Intrigued, the ${Quest.PLAYER1} learns of several conspiracies they have surpressed, not LEAST of which is the existance of ALIENS in the Medium. "),
                 new Quest("The ${Quest.PLAYER1} does a daring mission at a local Prospitian embassy and uncovers all sorts of juicy secrets. They can't help but read some of them ahead of time and- oh. Oh I see. In retrospect, the players kind of ARE aliens to the Medium. This conspiracy is a lot less interesting now.  "),
                 new Quest("The ${Quest.PLAYER1} shows the conspiracy papers to the three ${Quest.CONSORT}s, who fail to see the humor in the situation. They immediately try to rally their fellow ${Quest.CONSORT}s against the alien threat that is the foretold Players themselves. They...don't get even a single solitary ${Quest.CONSORTSOUND} of agreement from the crowd, though. Looks like they are stuck being the wacky conspiracy theorists. "),
-            ], new ConsortReward(), QuestChainFeature.defaultOption), Feature.WAY_LOW)
+            ], new RandomReward(), QuestChainFeature.defaultOption), Feature.WAY_LOW)
             ,  Theme.LOW);
     }
 
--- web/scripts/GameEntities/ClasspectStuff/Interests/Social.dart
+++ web/scripts/GameEntities/ClasspectStuff/Interests/Social.dart
@@ -49,7 +49,7 @@ class Social extends InterestCategory {
                 new Quest("The ${Quest.PLAYER1} finds a help wanted sign near a strange booth. Apparently the local ${Quest.CONSORT}s are in need of a therapist? The ${Quest.PLAYER1} decides to try it out! "),
                 new Quest("Huh, somehow all the ${Quest.CONSORT}'s problems end up being about childhood trauma involving ${Quest.MCGUFFIN} or ${Quest.PHYSICALMCGUFFIN}.  The ${Quest.PLAYER1} is getting really good at helping them out. "),
                 new Quest("A line of ${Quest.CONSORTSOUND}ing ${Quest.CONSORT} extends out to the horizon. The ${Quest.PLAYER1} is the most popular therapist on the planet! ")
-            ], new ConsortReward(), QuestChainFeature.defaultOption), Feature.WAY_LOW)
+            ], new RandomReward(), QuestChainFeature.defaultOption), Feature.WAY_LOW)
             ,  Theme.LOW);
 
         addTheme(new Theme(<String>["Meditation","Chants","Worship","Altars","Hymns", "Chapels", "Priests", "Angels", "Religion"])
@@ -64,7 +64,7 @@ class Social extends InterestCategory {
                 new Quest("The ${Quest.PLAYER1} wanders into an incredibly calm area of their land. It is filled with chanting and ${Quest.CONSORTSOUND}ing ${Quest.CONSORT}s. Apparently they are monks contemplating the vastness of the Vast Croak. The ${Quest.PLAYER1} joins them. "),
                 new Quest("While meditating with the ${Quest.CONSORT} monks, the ${Quest.PLAYER1} has come to a startling realization, the vast croak is related to ${Quest.MCGUFFIN}-ness. How could they miss this?"),
                 new Quest(" The ${Quest.PLAYER1} begins telling all the ${Quest.CONSORT} monks about how ${Quest.MCGUFFIN}-ness relates to the Vast Croak. The monks begin ${Quest.CONSORTSOUND}ing in amazement, this could revolutionize Frogism for generations!")
-            ], new ConsortReward(), QuestChainFeature.defaultOption), Feature.WAY_LOW)
+            ], new RandomReward(), QuestChainFeature.defaultOption), Feature.WAY_LOW)
             ,  Theme.LOW);
 
 
@@ -78,7 +78,7 @@ class Social extends InterestCategory {
                 new Quest("The ${Quest.PLAYER1} wanders into an entire crowd of ${Quest.CONSORT}s dressed in authentic style fantasy armor. What is going on? The nearest one stops ${Quest.CONSORTSOUND}ing long enough to explain that it's time for Paradox Space's biggest FLARP convention. The ${Quest.PLAYER1} happily joins the RP."),
                 new Quest("The ${Quest.PLAYER1}'s character in the FLARP is nearly maximum level. This is so much fun! Suddenly, a group of underlings attack the crowd. Confusion reigns as the ${Quest.CONSORT}s think it's somehow related to FLARP at first and don't fight back seriously. It is up to the ${Quest.PLAYER1} to save the day! All that RP practice surprisingly pays off. They win easily!   "),
                 new Quest("The FLARP is finally coming to an end. All the ${Quest.CONSORT}s agree that it is the best session in living memory and celebrate how the ${Quest.PLAYER1} kept it from becoming a tragedy.")
-            ], new ItemReward(items), QuestChainFeature.defaultOption), Feature.WAY_LOW)
+            ], new RandomReward(), QuestChainFeature.defaultOption), Feature.WAY_LOW)
             ,  Theme.LOW);
     }
 
