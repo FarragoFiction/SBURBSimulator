5819941e3a0ebc2ad9a8360dbd843d4dadde9c5b
Jenny <gtg287y@gmail.com>
2018-03-15 22:09:00 -0400

    almost ready to wire up

--- web/scripts/SessionEngine/CarapaceSummary.dart
+++ web/scripts/SessionEngine/CarapaceSummary.dart
@@ -3,12 +3,64 @@ import "../SBURBSim.dart";
 
 
 class CarapaceStats {
+
+    List<int> sessions = new List<int>();
+
     String initials;
+    String moon;
+
     //todo figure out how i want to collect stats.
-    int timesActivated = 0;
+    CarapaceStat timesActivated = new CarapaceStat("Times Activated");
+    CarapaceStat timesCrowned = new CarapaceStat("Times Crowned");
+    CarapaceStat carapacesMurdered = new CarapaceStat("Carapaces Murdered");
+    CarapaceStat moonsMurdered = new CarapaceStat("Moons Murdered");
+    CarapaceStat planetsMurdered = new CarapaceStat("Planets Murdered");
+    CarapaceStat redMilesUsed =  new CarapaceStat("Red Miles Activated");
+    CarapaceStat playersMurdered = new CarapaceStat("Players Murdered");
+    CarapaceStat timesDied = new CarapaceStat("Times Died");
+    CarapaceStat timesExiled = new CarapaceStat("Times Exiled");
+
+    List<CarapaceStat> get stats => <CarapaceStat>[timesActivated,timesCrowned,carapacesMurdered,moonsMurdered,planetsMurdered,redMilesUsed,playersMurdered,timesDied,timesExiled];
+
+
 
     CarapaceStats(Carapace carapace) {
         this.initials = carapace.initials;
+        this.moon = carapace.type;
+        sessions.addAll([13,8,4037,413,1025,612]);
+    }
+
+    Element makePortrait() {
+        DivElement div = new DivElement();
+        div.classes.add("cardPortrait");
+        div.style.backgroundImage = "url(images/BigBadCards/$moon.png)";
+
+        return div;
+    }
+
+    Element makeStats() {
+        DivElement div = new DivElement();
+        div.classes.add("cardStats");
+        for(CarapaceStat cs in stats) {
+            DivElement tmp = new DivElement();
+            SpanElement first = new SpanElement();
+            first.setInnerHtml("<b>${cs.name}</b>:");
+            SpanElement second = new SpanElement();
+            second.setInnerHtml("${cs.value}");
+            tmp.append(first);
+            tmp.append(second);
+            div.append(tmp);
+        }
+
+        div.appendHtml("Sessions Active In:");
+        for(int session_id in sessions) {
+            AnchorElement a = new AnchorElement();
+            a.href = "index2.html?seed=$session_id";
+            a.text = " $session_id, ";
+            div.append(a);
+        }
+
+        return div;
     }
 
     //display pic of prospit or derse.
@@ -25,7 +77,10 @@ class CarapaceStats {
         name.text = "Initials: $initials";
 
         divBorder.append(div);
+        div.append(makePortrait());
         div.append(name);
+        div.append(makeStats());
+
         return divBorder;
     }
 
@@ -50,4 +105,10 @@ class CarapaceSummary {
             }
         }
     }
+}
+
+class CarapaceStat {
+    String name;
+    int value;
+    CarapaceStat(this.name, [this.value=0]);
 }
