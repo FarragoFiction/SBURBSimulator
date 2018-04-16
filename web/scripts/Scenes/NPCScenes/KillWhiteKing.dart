c25fa4689a76866dfcfd17cab405453a550b7a6e
Jenny <gtg287y@gmail.com>
2018-04-16 14:12:07 -0400

    white king's inevitable defeat should be a thing

--- web/scripts/Scenes/NPCScenes/KillWhiteKing.dart
+++ web/scripts/Scenes/NPCScenes/KillWhiteKing.dart
@@ -6,19 +6,63 @@ import 'dart:html';
 class KillWhiteKing extends Scene {
     KillWhiteKing(Session session) : super(session);
 
-  @override
-  void renderContent(Element div) {
-      //;
-      gameEntity.available = false;
-      DivElement me = new DivElement();
-      me.setInnerHtml(getText());
-      div.append(me);
 
-  }
+    @override
+    void renderContent(Element div) {
+        gameEntity.available = false;
+        DivElement me = new DivElement();
+        div.append(me);
+        me.setInnerHtml(getText());
+        GameEntity wkowner = session.prospitScepter == null  ?  null:session.prospitScepter.owner;
 
-  String getText() {
-      print("$gameEntity wants to kill the white scepter owner in session ${session.session_id}");
-  }
+
+        if(wkowner != null) {
+            Element container = new DivElement();
+            me.append(container);
+            GameEntity whiteKing = session.battlefield == null  ?  null:session.battlefield.whiteKing;
+            startFight(div, wkowner, session.prospitScepter, whiteKing);
+        }
+    }
+
+
+
+
+    void startFight(Element container, GameEntity target, Scepter scepter, GameEntity whoSHOULDHaveIt) {
+        DivElement div = new DivElement();
+        container.append(div);
+        String text = "";
+        if(target == whoSHOULDHaveIt) {
+            text = "<br><br>It is time for the The ${target.htmlTitle()}'s inevitable defeat. ";
+        }else {
+            text = "<br><br>Huh. Well. The ${target.htmlTitle()} is DEFINITELY not the WHITE KING. You are blown away by this stunning revelation.  Who knew that the Main Quest of SBURB could go off the rails like that???  Either way, the scepters require a strife.";
+        }
+
+        div.setInnerHtml(text);
+
+        List<GameEntity> fighting = <GameEntity>[gameEntity];
+
+        for(GameEntity g in fighting) {
+            g.available = false;
+        }
+
+        Team pTeam = new Team.withName("The Owner of the ${session.derseScepter} ",this.session, fighting);
+        pTeam.canAbscond = false;
+        Team dTeam = new Team(this.session, [target]);
+        dTeam.canAbscond = false;
+        Strife strife = new Strife(this.session, [pTeam, dTeam]);
+        strife.timeTillRocks = 10;
+        strife.startTurn(div);
+
+        DivElement div2 = new DivElement();
+        container.append(div2);
+        div2.setInnerHtml("The ${scepter.owner.htmlTitle()} is now the owner of the ${scepter}. ");
+
+    }
+
+
+    String getText() {
+        return("<br><br>It is time. The ${gameEntity.htmlTitle()} feels the inexporable pull of the ${session.derseScepter.baseName} to slay whosoever bears the ${session.prospitScepter.baseName}.");
+    }
 
   @override
   bool trigger(List<Player> playerList) {
@@ -31,6 +75,8 @@ class KillWhiteKing extends Scene {
        */
       GameEntity bkowner = session.derseScepter == null  ?  null:session.derseScepter.owner;
       GameEntity wkowner = session.prospitScepter == null  ?  null:session.prospitScepter.owner;
+      //please don't try to murder yourself. it's fine.
+      if(bkowner == wkowner || wkowner == null) return false;
       print("should i try to kill the white king???");
       if(session.canReckoning && bkowner == gameEntity) return true;
       return false;
