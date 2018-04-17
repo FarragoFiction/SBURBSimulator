e0b82e46cb3445ae63b877fa75cd0737c1066996
Jenny <gtg287y@gmail.com>
2018-04-17 10:45:16 -0400

    have a solid idea of what needs to happen next, now I need to go back to the reckoning and bug fixes

--- web/scripts/GameEntities/BigBadStuff/TriggerCondition.dart
+++ web/scripts/GameEntities/BigBadStuff/TriggerCondition.dart
@@ -5,13 +5,45 @@
  */
 import "../../SBURBSim.dart";
 import 'dart:html';
-class TriggerCondition {
+abstract class TriggerCondition {
     static String BIGBADNAME = BigBad.BIGBADNAME;
     //could just be a carapace or a player I don't care
     GameEntity bigBad;
     //definitely replace this.
     String flavorText = "$BIGBADNAME suddenly appears for probably no reason.";
 
-    //TODO how to actually set up the triggers? sub classes?
+    //TODO how to actually set up the triggers? sub classes? where are my notes...
 
+    bool triggered();
+    void renderForm(Element div);
+
+    static SelectElement drawSelectTriggerConditions(Element div) {
+        //TODO call listPossibleTriggers to make the options, select teh first one by default.
+    }
+
+    static List<TriggerCondition> listPossibleTriggers() {
+        List<TriggerCondition> ret = new List<TriggerCondition>();
+        ret.add(new ItemTraitTriggerCondition());
+        //TODO should have drop down of one instance of each trigger, and a button to add a copy of one to the big bad
+        //todo once you've added the copy it renders itself as a sub form
+        return ret;
+    }
+
+}
+
+class ItemTraitTriggerCondition extends TriggerCondition{
+    static String BIGBADNAME = BigBad.BIGBADNAME;
+    static String ITEMTRAITNAME = "ITEMTRAITNAME";
+    ItemTrait itemTrait;
+    String flavorText = "$BIGBADNAME suddenly appears because one of the players is holding something that is $ITEMTRAITNAME.";
+
+  @override
+  bool triggered() {
+    // TODO: check sylladex and specibus of all players, do they have an item with this trait?
+  }
+  @override
+  void renderForm(Element div) {
+    // TODO: implement renderForm, should have list of all traits in system (do i even have that? oh uup)
+      Set<ItemTrait> allTraits = ItemTraitFactory.allTraits;
+  }
 }
