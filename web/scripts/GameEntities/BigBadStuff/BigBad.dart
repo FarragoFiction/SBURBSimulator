4dbd545cf00ba0ae262bce5d6b2d748149d70d6d
Jenny <gtg287y@gmail.com>
2018-04-19 09:19:55 -0400

    lots of big bad form work

--- web/scripts/GameEntities/BigBadStuff/BigBad.dart
+++ web/scripts/GameEntities/BigBadStuff/BigBad.dart
@@ -1,5 +1,7 @@
 import "../../SBURBSim.dart";
 import "dart:html";
+import '../../includes/lz-string.dart';
+import "dart:convert";
 /*
     Big Bads are serializable. Ironically not necessarily bad.
 
@@ -15,35 +17,93 @@ class BigBad extends NPC {
   //if any of these are true, the big bad is triggered.
   List<TriggerCondition> triggerConditions = new List<TriggerCondition>();
 
+  String get labelPattern => "$name:___ ";
+
   BigBad(String name, Session session) : super(name, session);
 
   String toDataString() {
-      //TODO use LZ compression
+      return  "$labelPattern${LZString.compressToEncodedURIComponent(toJSON().toString())}";
   }
 
   JSONObject toJSON() {
       JSONObject json = new JSONObject();
       json["name"] = name;
+      return json;
+  }
+
+  void copyFromDataString(String data) {
+      String dataWithoutName = data.split("$labelPattern")[1];
+      String rawJSON = LZString.decompressFromEncodedURIComponent(dataWithoutName);
+
+      JSONObject json = new JSONObject.fromJSONString(rawJSON);
+      name = json["name"];
   }
 
   static BigBad fromJSON(String rawJSON, Session session) {
-      JSONObject json = new JSONObject.fromJSONString(rawJSON);
-      BigBad ret = new BigBad(json["name"], session);
+      BigBad ret = new BigBad("TEMPORARY", session);
+      ret.copyFromDataString(rawJSON);
       return ret;
   }
+
+
 
   static BigBad fromDataString(String rawDataString) {
       //TODO use LZ compression
   }
 
   void drawForm(Element container) {
+      BigBadForm form = new BigBadForm(this, container);
 
-      LabelElement nameLabel = new LabelElement();
-      nameLabel.text = "Name:";
-      TextInputElement nameElement = new TextInputElement();
-      nameElement.value = name;
-      container.append(nameLabel);
-      container.append(nameElement);
   }
+
+
+
+}
+
+
+class BigBadForm {
+
+    Element container;
+    BigBad bigBad;
+
+    TextInputElement nameElement;
+    TextAreaElement dataBox;
+
+    BigBadForm(BigBad this.bigBad, Element this.container) {
+        drawForm();
+    }
+
+    void drawForm() {
+        drawName();
+        drawDataBox();
+    }
+
+    void syncDataBoxToBigBad() {
+        dataBox.value = bigBad.toDataString();
+    }
+
+    void drawName() {
+        LabelElement nameLabel = new LabelElement();
+        nameLabel.text = "Name:";
+        nameElement = new TextInputElement();
+        nameElement.value = bigBad.name;
+        container.append(nameLabel);
+        container.append(nameElement);
+
+        nameElement.onInput.listen((e) {
+            bigBad.name = nameElement.value;
+            syncDataBoxToBigBad();
+        });
+    }
+
+    void drawDataBox() {
+        dataBox = new TextAreaElement();
+        dataBox.value = bigBad.toDataString();
+        dataBox.onChange.listen((e) {
+            print("syncing template to data box");
+            bigBad.copyFromDataString(dataBox.value);
+        });
+        container.append(dataBox);
+    }
 
 }
