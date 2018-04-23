28218e912fb1fb20d64c2eb6667d1d0cee8ec72c
Jenny <gtg287y@gmail.com>
2018-04-23 09:20:53 -0400

    more big bad work

--- web/scripts/GameEntities/BigBadStuff/TriggerCondition.dart
+++ web/scripts/GameEntities/BigBadStuff/TriggerCondition.dart
@@ -6,11 +6,13 @@
 import "../../SBURBSim.dart";
 import 'dart:html';
 abstract class TriggerCondition {
+    Session session;
     static String BIGBADNAME = BigBad.BIGBADNAME;
     //could just be a carapace or a player I don't care
     GameEntity bigBad;
     //definitely replace this.
     String flavorText = "$BIGBADNAME suddenly appears for probably no reason.";
+    TriggerCondition(Session this.session);
 
     //TODO how to actually set up the triggers? sub classes? where are my notes...
 
@@ -21,9 +23,11 @@ abstract class TriggerCondition {
         //TODO call listPossibleTriggers to make the options, select teh first one by default.
     }
 
-    static List<TriggerCondition> listPossibleTriggers() {
+    static List<TriggerCondition> listPossibleTriggers(Session session) {
         List<TriggerCondition> ret = new List<TriggerCondition>();
-        ret.add(new ItemTraitTriggerCondition());
+        ret.add(new ItemTraitTriggerCondition(session));
+        ret.add(new CrownedCarapaceTriggerCondition(session));
+
         //TODO should have drop down of one instance of each trigger, and a button to add a copy of one to the big bad
         //todo once you've added the copy it renders itself as a sub form
         return ret;
@@ -40,7 +44,10 @@ class ItemTraitTriggerCondition extends TriggerCondition{
     ItemTrait itemTrait;
     //strongly encouraged for this to be replaced
     //like, "An ominous 'honk' makes the Knight of Rage drop the Juggalo Poster in shock. With growing dread they realize that shit is about to get hella rowdy, as the Mirthful Messiahs have rolled into town.
+    @override
     String flavorText = "$BIGBADNAME suddenly appears because the ${ITEMTRAITOWNERNAME} is holding a ${ITEMAME} that is $ITEMTRAITNAME.";
+
+  ItemTraitTriggerCondition(Session session) : super(session);
 
   @override
   bool triggered() {
@@ -51,4 +58,31 @@ class ItemTraitTriggerCondition extends TriggerCondition{
     // TODO: implement renderForm, should have list of all traits in system (do i even have that? oh uup)
       Set<ItemTrait> allTraits = ItemTraitFactory.allTraits;
   }
+}
+
+class CrownedCarapaceTriggerCondition extends TriggerCondition {
+    static String BIGBADNAME = BigBad.BIGBADNAME;
+
+    static String CARAPACENAME = "CARAPACENAME";
+    static String CROWNNAME = "CROWNNAME";
+
+    String carapaceInitials;
+    //strongly encouraged for this to be replaced
+    //like, "An ominous 'honk' makes the Knight of Rage drop the Juggalo Poster in shock. With growing dread they realize that shit is about to get hella rowdy, as the Mirthful Messiahs have rolled into town.
+    @override
+    String flavorText = "$BIGBADNAME suddenly appears because the ${CARAPACENAME} has summoned them with the ULTIMATE POWER a ${CROWNNAME} represents.";
+
+  CrownedCarapaceTriggerCondition(Session session) : super(session);
+
+    @override
+    bool triggered() {
+        // TODO: check session active npcs, does initials have crown?
+    }
+    @override
+    void renderForm(Element div) {
+        // TODO: implement renderForm, should have list of all carapace initials
+        List<GameEntity> allCarapaces = new List.from(session.prospit.associatedEntities);
+        allCarapaces.addAll(session.derse.associatedEntities);
+        //TODO
+    }
 }
