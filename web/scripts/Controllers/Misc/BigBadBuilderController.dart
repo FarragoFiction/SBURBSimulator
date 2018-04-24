b2f4d808fb6bf59cd4075f3c33dd0c952552f212
Jenny <gtg287y@gmail.com>
2018-04-24 14:54:29 -0400

    nothing to see here

--- web/scripts/Controllers/Misc/BigBadBuilderController.dart
+++ web/scripts/Controllers/Misc/BigBadBuilderController.dart
@@ -16,13 +16,17 @@ void main() {
   todoElement.style.border = "1px";
   div.append(todoElement);
 
+  todo("trigger condition can be 'any' ");
+  todo("big bads have intro mod flavor text (like dystopic empire)");
   todo("scenes should flavor text with replaceable words and shit. (how to do with such complex triggers?)");
+  todo("split main and experimental, test spawning big bads. they won't DO anything yet, but thats okay");
   todo("big bads need 0 or more custom fraymotif names");
   todo("big bads need low, medium, high values for all stats. what am i saying i mean 'Planetary, Galactic, Cosmic'. sorry about that, shogun");
   todo("big bad needs to be in default state before loading still, need to refresh page to clear shit out");
-  todo("big bads have intro mod flavor text (like dystopic empire)");
   todo("big bads have outro mod flavor text (if they aren't defeated, how do they effect child universe, like dystopic empire)");
   todo("scenes shouldu have effects styled just like triggers");
+  todo("way to test");
+  todo("side apps, like big bad gotcha, or big bad betting battles");
 
   setUpForm();
 
--- web/scripts/GameEntities/ClasspectStuff/Classes/Bard.dart
+++ web/scripts/GameEntities/ClasspectStuff/Classes/Bard.dart
@@ -70,7 +70,7 @@ class Bard extends SBURBClass {
     void initializeItems() {
         items = new WeightedList<Item>()
         //things that let you destroy yourself.
-            ..add(new Item("Cod Piece",<ItemTrait>[ItemTraitFactory.CLOTH,ItemTraitFactory.LEGENDARY,ItemTraitFactory.FAKE, ItemTraitFactory.CLASSRELATED],abDesc:"God damn it, MI. "))
+            ..add(new Item("Cod Piece",<ItemTrait>[ItemTraitFactory.CLOTH,ItemTraitFactory.LEGENDARY,ItemTraitFactory.FAKE, ItemTraitFactory.CLASSRELATED, ItemTraitFactory.WOOD],abDesc:"God damn it, MI. "))
             ..add(new Item("Poisoned Candy",<ItemTrait>[ItemTraitFactory.CANDY, ItemTraitFactory.CLASSRELATED, ItemTraitFactory.POISON],shogunDesc: "Not So Sweet Treat",abDesc:"I guess CodTier is okay."))
             ..add(new Item("Cursed Lyre",<ItemTrait>[ItemTraitFactory.DOOMED,ItemTraitFactory.WOOD,ItemTraitFactory.CALMING, ItemTraitFactory.CLASSRELATED, ItemTraitFactory.MUSICAL],shogunDesc: "I Don’t Know What This Is Normally",abDesc:"I guess CodTier is okay. Sort of."))
             ..add(new Item("Snare Trap",<ItemTrait>[ItemTraitFactory.CLOTH,ItemTraitFactory.DOOMED, ItemTraitFactory.CLASSRELATED, ItemTraitFactory.RESTRAINING],shogunDesc: "The Perfect Trap",abDesc:"I guess CodTier is okay. But still. The actual codpiece. You fleshy meatbags and your weird shit."));
