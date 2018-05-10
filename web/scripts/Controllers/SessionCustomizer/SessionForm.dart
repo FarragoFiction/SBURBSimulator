4f243e9dff6c343421e024dcc1d7e9c2d004ddc1
Jenny <gtg287y@gmail.com>
2018-05-10 11:12:47 -0400

    should be able to set big bads for a session

--- web/scripts/Controllers/SessionCustomizer/SessionForm.dart
+++ web/scripts/Controllers/SessionCustomizer/SessionForm.dart
@@ -13,7 +13,46 @@ class SessionForm {
     }
 
     void draw() {
-        container.setInnerHtml("TODO: draw form");
+        container.setInnerHtml("Customize Session ${session.session_id}");
+        drawBigBadTextBox();
+    }
+
+    void drawBigBadTextBox() {
+        DivElement badContainer = new DivElement();
+
+
+        container.append(badContainer);
+        LabelElement label = new LabelElement()..setInnerHtml("Add One Big Bad Builder DataString to Test:");
+        badContainer.append(label);
+        TextAreaElement badArea = new TextAreaElement();
+        badArea.cols = 60;
+        badArea.rows = 10;
+        badContainer.append(badArea);
+
+        DivElement checkBoxContainer = new DivElement();
+        badContainer.append(checkBoxContainer);
+        LabelElement labelCheckBox = new LabelElement()..setInnerHtml("Should they Spawn Active?:");
+        checkBoxContainer.append(labelCheckBox);
+        CheckboxInputElement isActive = new CheckboxInputElement();
+        checkBoxContainer.append(isActive);
+
+        DivElement buttonContainer = new DivElement();
+        ButtonElement buttonElement = new ButtonElement()..text = "Load Big Bad";
+        badContainer.append(buttonContainer);
+        buttonContainer.append(buttonElement);
+
+        buttonElement.onClick.listen((MouseEvent e) {
+            window.alert("todo");
+            BigBad bigBad = new BigBad("N/A", session);
+            bigBad.copyFromDataString(badArea.value);
+            if(isActive.checked) {
+                bigBad.active = true;
+            }
+            session.bigBads.add(bigBad);
+        });
+
+
+
     }
 
 }
