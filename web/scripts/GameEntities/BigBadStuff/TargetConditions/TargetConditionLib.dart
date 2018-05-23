feac3a9f802667f6e3524ab6eca95772cf3b92da
Jenny <gtg287y@gmail.com>
2018-05-23 19:50:33 -0400

    real name for file typo fix

--- web/scripts/GameEntities/BigBadStuff/TargetConditions/TargetConditionLib.dart
+++ web/scripts/GameEntities/BigBadStuff/TargetConditions/TargetConditionLib.dart
@@ -6,6 +6,6 @@ export "TargetHasItemWithTrait.dart";
 export "TargetHasCrown.dart";
 export "TargetIsAlive.dart";
 export "TargetIsPlayer.dart";
-export "BLOWAWAY.dart";
+export "TargetIsAspectPlayer.dart";
 export "TargetIsClassPlayer.dart";
 export "TargetIsSelf.dart";
