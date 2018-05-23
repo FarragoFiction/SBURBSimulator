528ab46a0e76d3f3f0916ed1641b803b0e42769b
Jenny <gtg287y@gmail.com>
2018-05-23 09:03:31 -0400

    can load target aspect player from json

--- web/scripts/GameEntities/BigBadStuff/TargetConditions/TargetisAspectPlayer.dart
+++ web/scripts/GameEntities/BigBadStuff/TargetConditions/TargetisAspectPlayer.dart
@@ -4,25 +4,35 @@ import 'dart:html';
 class TargetIsAspectPlayer extends TargetConditionLiving {
   TargetIsAspectPlayer(SerializableScene scene) : super(scene);
 
-  Aspect aspect;
+  String aspectName;
+  List<String> _allAspects  = new List<String>();
+
+  List<String> get allAspects {
+      //print("getting allTraits");
+      if(_allAspects == null || _allAspects.isEmpty) {
+          _allAspects = Aspects.names;
+      }
+      return _allAspects;
+  }
+
 
   @override
   String name = "isAspectPlayer";
 
   @override
-  String get importantWord => "$aspect";
+  String get importantWord => "$aspectName";
 
 
   @override
   void copyFromJSON(JSONObject json) {
-    // TODO: implement copyFromJSON
+      aspectName = json[TargetCondition.IMPORTANTWORD];
   }
 
   @override
   List<GameEntity> filter(List<GameEntity> list) {
       list.removeWhere((GameEntity entity) {
           if (entity is Player) {
-              if((entity as Player).aspect == aspect) {
+              if((entity as Player).aspect.name == aspectName) {
                   return false; //don't remove if i'm this aspect
               }else {
                   return true;
