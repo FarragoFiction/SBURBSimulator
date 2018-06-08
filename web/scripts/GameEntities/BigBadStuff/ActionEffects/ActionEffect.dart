b336724ab599e065747fc420c81de03186a83b11
jadedResearcher <gtg287y@gmail.com>
2018-06-08 15:05:57 -0400

    quest shit is done and definitely didn't take an hour

--- web/scripts/GameEntities/BigBadStuff/ActionEffects/ActionEffect.dart
+++ web/scripts/GameEntities/BigBadStuff/ActionEffects/ActionEffect.dart
@@ -158,6 +158,10 @@ abstract class EffectEntity extends ActionEffect {
       ret.add(new GiveAction(scene));
       ret.add(new PickPocket(scene));
       ret.add(new Mug(scene));
+      ret.add(new GiveFrog(scene));
+      ret.add(new MarkFirstQuestsAsComplete(scene));
+      ret.add(new MarkDenizenQuestsAsComplete(scene));
+      ret.add(new MarkAllQuestsAsComplete(scene));
       ret.add(new PledgeLoyalty(scene));
       ret.add(new MakeMinion(scene));
       ret.add(new MakeGodDestined(scene));
