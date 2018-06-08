ace84f7562cf4a4aa0b73891bf3be02800b79be2
jadedResearcher <gtg287y@gmail.com>
2018-06-08 15:02:15 -0400

    eheheheeh if you give a grim dark player a frog you're gonna have a bad time

---
+++ web/scripts/GameEntities/BigBadStuff/ActionEffects/EntityEffects/MarkFirstQuestsAsComplete.dart
@@ -0,0 +1,57 @@
+import "../../../../SBURBSim.dart";
+import 'dart:html';
+
+//no chance to survive, no strife no anything. it's a red miles situation
+//not really any details, or modifiers
+class MarkFirstQuestsAsComplete extends EffectEntity {
+    @override
+    String name = "MarkFirstQuestsAsComplete";
+    MarkFirstQuestsAsComplete(SerializableScene scene) : super(scene);
+
+
+  @override
+  void copyFromJSON(JSONObject json) {
+    // nothing to do
+  }
+
+  @override
+  void syncFormToMe() {
+    ////does nothing since i have no personal data
+  }
+
+    @override
+    void renderForm(Element divbluh) {
+        setupContainer(divbluh);
+
+        DivElement me = new DivElement();
+        container.append(me);
+        me.setInnerHtml("<b>MarkFirstQuestsAsComplete:</b> <br>a player's first set of quests are marked as complete (but without getting the rewards and exp from them). <br><br>");
+        syncToForm();
+    }
+
+  @override
+  void syncToForm() {
+      scene.syncForm();
+  }
+  @override
+  void effectEntities(List<GameEntity> entities) {
+      List<GameEntity> renderableTargets = new List<GameEntity>();
+    entities.forEach((GameEntity e) {
+        if(e.renderable()) renderableTargets.add(e);
+        if(e is Player) {
+          Land land = (e as Player).land;
+          if(land != null) land.firstCompleted = true;
+        }
+    });
+    if(renderableTargets.isNotEmpty && !scene.posedAsATeamAlready) {
+        CanvasElement canvasDiv = new CanvasElement(width: canvasWidth, height: canvasHeight);
+        scene.myElement.append(canvasDiv);
+        Drawing.poseAsATeam(canvasDiv, renderableTargets);
+        scene.posedAsATeamAlready = true;
+    }
+  }
+  @override
+  ActionEffect makeNewOfSameType() {
+    return new MarkFirstQuestsAsComplete(scene);
+  }
+}
