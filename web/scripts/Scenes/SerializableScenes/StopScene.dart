79b65600483a1b0073e646edf2f64e9002c1b34d
jadedResearcher <gtg287y@gmail.com>
2018-06-26 14:03:19 -0400

    small refactor

--- web/scripts/Scenes/SerializableScenes/StopScene.dart
+++ web/scripts/Scenes/SerializableScenes/StopScene.dart
@@ -17,17 +17,38 @@ class StopScene extends SerializableScene {
     bool trigger(List<Player> playerList) {
         //session.logger.info("TEST BIG BAD: checking triggers");
         posedAsATeamAlready = false;
+        landTargets.clear();
         livingTargets.clear();
-        if(originalOwner == null || originalOwner.dead || !originalOwner.active) return false;
+        if(originalOwner.dead) return false;
+        if(!originalOwner.active) return false;
         livingTargets.add(originalOwner);
+        //TODO should i also get party members for those npcs? otherwise can't get brain ghosts and robots and the like
+        for(Player p in session.players) {
+            if(p.active) {
+                landTargets.add(p.land);
+            }
+        }
 
+        //session.logger.info("i think active targets is $livingTargets");
 
-        //still can have normal targets, so defeat conditions can trigger with diff things
+        landTargets.addAll(session.moons);
+
+        landTargets = new Set<Land>.from(shuffle(session.rand, new List<Land>.from(landTargets)));
+
         for(TargetConditionLiving tc in triggerConditionsLiving) {
             livingTargets = new Set<GameEntity>.from(tc.filter(new List<GameEntity>.from(livingTargets)));
         }
         if(triggerConditionsLiving.isEmpty) livingTargets.clear();
-        return livingTargets.isNotEmpty;
+
+
+        for(TargetConditionLand tc in triggerConditionsLand) {
+            landTargets = new Set<Land>.from(tc.filter(new List<Land>.from(landTargets)));
+        }
+        if(triggerConditionsLand.isEmpty) landTargets.clear();
+        //even if you have land targets, NEED to target the original owner
+        if(livingTargets.isEmpty) return false;
+
+        return landTargets.isNotEmpty || livingTargets.isNotEmpty;
     }
 
 }
