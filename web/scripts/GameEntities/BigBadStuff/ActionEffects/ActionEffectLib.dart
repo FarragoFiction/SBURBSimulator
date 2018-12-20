3529f96d5ccfbd59709c86c863be57d5fbf30f60
jadedResearcher <gtg287y@gmail.com>
2018-12-19 21:35:21 -0500

    require and unrequire effects

--- web/scripts/GameEntities/BigBadStuff/ActionEffects/ActionEffectLib.dart
+++ web/scripts/GameEntities/BigBadStuff/ActionEffects/ActionEffectLib.dart
@@ -11,6 +11,8 @@ export "EntityEffects/KillSleepingDreamSelf.dart";
 export "EntityEffects/Mug.dart";
 export "EntityEffects/BanScene.dart";
 export "EntityEffects/UnBanScene.dart";
+export "EntityEffects/RequireScene.dart";
+export "EntityEffects/UnRequireScene.dart";
 
 export "EntityEffects/AntiMug.dart";
 export "EntityEffects/GiveItem.dart";
